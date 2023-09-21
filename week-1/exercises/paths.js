962177625f432e5ccac0b395f43ac52f7f77e970
= <adniyayousaf92@gmail.com>
2023-09-21 14:12:38 +0100

    completed paths.js

--- week-1/exercises/paths.js
+++ week-1/exercises/paths.js
@@ -14,5 +14,8 @@ const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
+const store=base.slice(-4);
+console.log(store);
+
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
