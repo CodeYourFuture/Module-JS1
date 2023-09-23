a2e627a4c2f312ce10b86b3d0b88e28dfa437b9a
Aisha Athman <aishaathmanal@gmail.com>
2023-09-23 15:40:25 +0100

    add answer to count.js

--- week-1/exercises/count.js
+++ week-1/exercises/count.js
@@ -4,3 +4,5 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+= is giving value to the variable count as 0
+line 3 which is {count = count +1} is assigning new val;ue to the variable count whichi is 0 + 1.
