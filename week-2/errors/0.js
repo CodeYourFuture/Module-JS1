6205466945c80c90272e88f106fb139f3a6c2902
Marcus <marcus.ats94@gmail.com>
2023-09-24 20:44:41 +0100

    Completed week-2 debug and errors. Apologies for not pushing seperately

--- week-2/errors/0.js
+++ week-2/errors/0.js
@@ -1,9 +1,18 @@
 // Predict and explain first...
 
+// Because str has been stored as a value inside the function capitalize, meaning that it will not work,
+// once a name has been used it cannot be reused as this will cause conflict.
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let capitalizedStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalizedStr;
 }
+
+const capitaliseTest = capitalise("hello");
+const myNameCapTest = capitalise("marcus");
+
+console.log(capitaliseTest);
+console.log(myNameCapTest);
