5538fdc646164af749cbae0e55c7d2077302b3e7
bahador <noor.bahadory786@gmail.com>
2023-09-26 21:09:17 +0100

    started exercises count and decimal

--- week-1/exercises/decimal.js
+++ week-1/exercises/decimal.js
@@ -1,5 +1,12 @@
 const num = 58.4567;
 
+const wholeNumberPart = Math.floor(num) - 2;
+console.log(wholeNumberPart);
+
+const decimalPart = num - wholeNumberPart;
+const roundedNum = Math.round(num);
+console.log(wholeNumberPart, decimalPart, roundedNum);
+
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 // Create a variable called wholeNumberPart and assign to it an expression that evaluates to 56 ( the whole number part of num )
@@ -7,3 +14,13 @@ const num = 58.4567;
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
 
 // Log your variables to the console to check your answers
+
+//const num = 58.4567;
+
+//const wholeNumberPart = Math.round(num) - 2;
+//const decimalPart = num - 58;
+//const roundedNum = Math.round(num) - 1;
+
+//console.log(wholeNumberPart);
+//console.log(decimalPart);
+//console.log(roundedNum);
