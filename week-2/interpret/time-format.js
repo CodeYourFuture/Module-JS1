39f8432d825f899ee11aa481a03bd8920885e1c5
Marcus <marcus.ats94@gmail.com>
2023-09-26 17:47:05 +0100

    Completed the interpret task time-format

--- week-2/interpret/time-format.js
+++ week-2/interpret/time-format.js
@@ -9,6 +9,8 @@ function formatTimeDisplay(seconds) {
   const totalHours = (totalMinutes - remainingMinutes) / 60;
   const remainingHours = totalHours % 24;
 
+  console.log(remainingHours, remainingSeconds, totalHours);
+
   return `${pad(remainingHours)}:${pad(remainingMinutes)}:${pad(
     remainingSeconds
   )}`;
@@ -22,15 +24,24 @@ console.log(formatTimeDisplay(61));
 // Questions
 
 // a) Run this program with node, identify the line the error is thrown. Explain why this error is occurring. How can you fix this error?
+// There is no errors?
 
 // b) When formatTimeDisplay is called how many times will pad be called?
+// 3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // c) What is the value assigned to num when pad is called for the first time?
+// remainingHours
 
 // d) What is the return value of pad is called for the first time?
+// 0
 
 // e) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
+// remainingSeconds - Because once the variables have been defined inside of the function, there must be a return output,
+// So for example - The variable and code you'd like the execute will be the input, and the return will be the output.
+// Pad is called inside of the return line on line 12, and the last variable it asks to call is remainingSeconds.
 
 // f) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+// 1 - Because seconds % 60 is a modulo calculation. The seconds is the dividend and the 60 is the divisor. When this sum
+// is calculated it equals 1.
