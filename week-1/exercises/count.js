91910efcae1f927bdf5940f7fe8df0054db0e8ac
berwin eibo <perween.kurd1@gmail.com>
2023-10-02 23:08:51 +0100

    solve exercise 1

--- week-1/exercises/count.js
+++ week-1/exercises/count.js
@@ -4,3 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+it is increasing the value of the count vaiable which is 0 by 1
