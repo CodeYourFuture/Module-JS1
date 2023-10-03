b3ae6d71e78936b6105086300ff1a9738808d058
karimi <behrouzkarimi1993@gmail.com>
2023-10-03 02:41:40 +0100

    3.js errors

--- week-1/errors/3.js
+++ week-1/errors/3.js
@@ -1,7 +1,13 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = cardNumber.toString().slice(-4);
+console.log(last4Digits);
+
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
 // Make and explain a prediction about why the code won't work
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  ===========>>>>>>>>>>>>>>>>>>>>>
+//.slice() method is for objects and strings not a number , by changing cardNumber to an string by..
+// toString() method we are allowed to use .slice() .
