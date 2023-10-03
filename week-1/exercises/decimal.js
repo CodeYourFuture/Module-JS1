cfb951251bb37260ab0a77af5976556bca40da81
karimi <behrouzkarimi1993@gmail.com>
2023-10-03 01:07:40 +0100

    decimal.js solving

--- week-1/exercises/decimal.js
+++ week-1/exercises/decimal.js
@@ -1,4 +1,4 @@
-const num = 58.4567;
+const num = -58.4567;
 
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
@@ -7,3 +7,15 @@ const num = 58.4567;
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
 
 // Log your variables to the console to check your answers
+
+//note in second line there's a typo and whole number should be 58 not 56 . 
+let wholeNumberPart=Math.trunc(num);
+console.log(wholeNumberPart);
+
+let decimalPart=num-Math.floor(num); //this expression doesn't work on negative numbers. and also return an estimated value of floated part.
+let decimalPart2=num.toString().substring(num.toString().indexOf(".")+1); //this one works better lol
+console.log(decimalPart);
+console.log(decimalPart2);
+
+let roundedNum=Math.round(num);
+console.log(roundedNum)
