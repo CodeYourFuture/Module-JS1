e8460aed0504640c5fdd6a90e7f4d02802f89dce
berwin eibo <perween.kurd1@gmail.com>
2023-10-03 19:05:48 +0100

    errors solved

--- week-1/errors/4.js
+++ week-1/errors/4.js
@@ -1,2 +1,6 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const $12HourClockTime = "20:53";
+const $24HourClockTime = "08:53";
+console.log($24HourClockTime);
+console.log($12HourClockTime);
+
+// invalid variable name that start with a digit which is not allowed in JS. we should use $ or _ or a letter at the start of the variable or following it.
