8d46d1a478cbd538417fd85c45ab9b4f4a2d508d
karimi <behrouzkarimi1993@gmail.com>
2023-10-03 02:48:50 +0100

    js.4 errors

--- week-1/errors/4.js
+++ week-1/errors/4.js
@@ -1,2 +1,10 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+
+//corrected example =>
+const _12HourClockTime = "20:53";
+const _24hourClockTime = "08:53";
+
+// variable names can not start with numbers. also other characters like / @ and so on .
+// a variable can start and also with [a-z] , [A-Z] and also _ (underline) and $ (dollar sign)
+// a variable name can contain a-z  , A-Z , numbers , _ and $
