5171408b7dff449d4c60c473fb78b936772f29f9
karimi <behrouzkarimi1993@gmail.com>
2023-10-03 01:13:34 +0100

    initial.js

--- week-1/exercises/initials.js
+++ week-1/exercises/initials.js
@@ -4,3 +4,9 @@ let lastName = "Johnson";
 
 // Declare a variable called initials that stores the first 3 characters of each string in upper case
 // Log the variable in each case
+
+let initial=firstName.substring(0,3).toUpperCase()+
+middleName.substring(0,3).toUpperCase()+
+lastName.substring(0,3).toUpperCase();
+
+console.log(initial);
