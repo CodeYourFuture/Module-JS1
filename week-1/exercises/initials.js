031a748da48e760817d379d9fb3d103da1ba73b6
Zahra-Khademi <zahrakhademi420@gmail.com>
2023-10-03 19:06:55 +0100

    doing the tasks

--- week-1/exercises/initials.js
+++ week-1/exercises/initials.js
@@ -4,3 +4,11 @@ let lastName = "Johnson";
 
 // Declare a variable called initials that stores the first 3 characters of each string in upper case
 // Log the variable in each case
+
+let firstInitials = firstName.substring(0, 3).toUpperCase();
+let middleInitials = middleName.substring(0, 3).toUpperCase();
+let lastInitials = lastName.substring(0, 3).toUpperCase();
+
+let initials = firstInitials + middleInitials + lastInitials;
+
+console.log(initials);
