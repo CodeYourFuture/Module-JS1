09ba98eb3a35a04fb3f0e2468c3591cf394425d5
karimi <behrouzkarimi1993@gmail.com>
2023-10-03 02:31:10 +0100

    2.js errors

--- week-1/errors/2.js
+++ week-1/errors/2.js
@@ -1,5 +1,10 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+
+
+// ReferenceError: Cannot access 'cityOfBirth' before initialization
+// cityOfBirth should've been declared before uses
+
