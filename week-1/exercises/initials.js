7a8e7386f9f70cabf88e4a1759ff39d46c567436
aishaathmanlali <aishaathmanal@gmail.com>
2023-10-04 18:32:55 +0100

    update answer

--- week-1/exercises/initials.js
+++ week-1/exercises/initials.js
@@ -5,9 +5,5 @@ let lastName = "Johnson";
 // Declare a variable called initials that stores the first 3 characters of each string in upper case
 // Log the variable in each case
 
-let initials = firstName.slice(0, 3).toUpperCase();
-console.log(initials);
-let initials = middleName.slice(0, 3).toUpperCase();
-console.log(initials);
-let initials = lastName.slice(0, 3).toUpperCase();
+const initials = firstName.slice(0, 3).toUpperCase() + " " + middleName.slice(0, 3).toUpperCase() + " " + lastName.slice(0, 3).toUpperCase();
 console.log(initials);
