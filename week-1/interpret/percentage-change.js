54fac4bbf71fea9545c12121ef9a4a174780fcab
LauraSantiag0 <eusoulaura8@gmail.com>
2023-10-04 14:25:19 +0100

    final

--- week-1/interpret/percentage-change.js
+++ week-1/interpret/percentage-change.js
@@ -9,14 +9,42 @@ const percentageChange = (priceDifference / carPrice) * 100;
 
 console.log(`The percentage change is ${percentageChange}`);
 
+/*
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
+There are three function calls in this code:
+
+carPrice.replaceAll(",", "") on line 4
+priceAfterOneYear.replaceAll(",", "") on line 5
+Number() used twice on lines 4 and 5
+
+
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+
+
 
 // c) Identify all the lines that are variable reassignment statements
 
+The lines that are variable reassignment statements are:
+
+carPrice = Number(carPrice.replaceAll(",", "")); on line 4
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", "")); on line 5
+
 // d) Identify all the lines that are variable declarations
 
+the lines that are variable declarations are:
+
+let carPrice = "10,000"; on line 1
+let priceAfterOneYear = "8,543"; on line 2
+const priceDifference = carPrice - priceAfterOneYear; on line 6
+const percentageChange = (priceDifference / carPrice) * 100; on line 7
+
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+The expression Number(carPrice.replaceAll(",", "")) is converting a string that represents a number with commas as a thousands separator (e.g., "10,000") into an actual JavaScript number. To do this, it first uses replaceAll to remove any commas from the string, and then it uses Number() to convert the resulting string into a numeric data type. The purpose of this expression is to ensure that carPrice and priceAfterOneYear are treated as numeric values rather than strings, so mathematical operations like subtraction and division can be performed on them.
+
+
+
+
--- week-1/interpret/to-pounds.js
+++ week-1/interpret/to-pounds.js
@@ -11,11 +11,32 @@ const pounds = paddedPenceNumberString.substring(
   paddedPenceNumberString.length - 2
 );
 
+
+
+
 const pence = paddedPenceNumberString
   .substring(paddedPenceNumberString.length - 2)
   .padEnd(2, "0");
 
+//399
+
 console.log(`£${pounds}.${pence}`);
+
+
+/*
+line 1) This line initializes a constant variable penceString and assigns it the value "399p", which represents a price in pence.
+
+line 3 ) This line creates a new string penceStringWithoutTrailingP by using the substring method to remove the last character (which is 'p') from the penceString. The resulting string is "399".
+
+line 8)Here, paddedPenceNumberString is created by using the padStart method. This method pads the penceStringWithoutTrailingP with zeros at the beginning to make sure it has a minimum length of 3 characters. Since "399" already has 3 characters, no padding is added, and the result is still "399".
+
+line 9)This line extracts the pound portion of the paddedPenceNumberString. It uses substring to take all characters from the start up to the third-to-last character, effectively removing the last two characters. In this case, it extracts "3".
+
+line 17)Here, pence is created by extracting the pence portion of the paddedPenceNumberString. It uses substring to get the last two characters, which are "99". Then, it uses padEnd to ensure that the pence string has a length of 2 characters by adding zeros if necessary. So, "99" remains unchanged.
+
+line 23) 
+this line prints the price in pounds and pence to the console using template literals. It combines the pounds and pence variables to create a string in the format £3.99, which is then displayed in the console.
+
 
 // This program takes a string representing a price in pence
 // The program then builds up a string representing the price in pounds
