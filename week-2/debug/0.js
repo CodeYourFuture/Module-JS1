bcfff83419bcf17f1e5916be626bbe1d98e630a0
berwin eibo <perween.kurd1@gmail.com>
2023-10-05 23:12:57 +0100

    explained the use of multiply function

--- week-2/debug/0.js
+++ week-2/debug/0.js
@@ -1,7 +1,10 @@
 // Predict and explain first...
 
 function multiply(a, b) {
-  console.log(a * b);
+  console.log(a * b); 
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// the function multiply logs the result of the multiplication of a and b to the console but does not return any Value.
+// console.log: to output the result of 10*32 by using the multiply function.
