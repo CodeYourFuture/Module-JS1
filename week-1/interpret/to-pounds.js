d308e8cf74061ebcd569ebcc2ea664369370b6d8
berwin eibo <perween.kurd1@gmail.com>
2023-10-05 21:12:46 +0100

    interpret/to-pound solved

--- week-1/interpret/to-pounds.js
+++ week-1/interpret/to-pounds.js
@@ -25,3 +25,10 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 2- onst penceStringWithoutTrailingP = penceString.substring(0,penceString.length - 1);
+// creates a new string variable called penceStringWithoutTrailingP by using the substring method to remove the last character (which is "p") from the penceString
+// const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");: Here, we create another string variable called paddedPenceNumberString. It uses the padStart method to add leading zeros to make the string 3 characters long
+// 
+// const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");: Here, we create a string variable called pence. It extracts the last two characters from paddedPenceNumberString, representing pence. Then, it uses padEnd to ensure that pence is exactly 2 characters long. So, "399" becomes "99", and after padding, it's still "99".
+
+// console.log(£${pounds}.${pence}); logs the formatted price to the console
