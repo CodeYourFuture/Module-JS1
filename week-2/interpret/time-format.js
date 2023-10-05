8116030a8b0743bd1ea528378e18f6fa6dfd4ab0
Tajudeen Oyehan <oyehan.uob@gmail.com>
2023-10-05 02:41:53 +0100

    time-format interpretedI

--- week-2/interpret/time-format.js
+++ week-2/interpret/time-format.js
@@ -22,15 +22,23 @@ console.log(formatTimeDisplay(61));
 // Questions
 
 // a) Run this program with node, identify the line the error is thrown. Explain why this error is occurring. How can you fix this error?
+// I ran the program. It works fine.
 
 // b) When formatTimeDisplay is called how many times will pad be called?
+// The function, pad, will be called 3 times.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // c) What is the value assigned to num when pad is called for the first time?
+// The first time pad is called, num will be the remainingHours and value is 0, and it will return 00.
 
 // d) What is the return value of pad is called for the first time?
+// The return value will be 00.
 
 // e) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
+// The last time pad is called, it is called on remainingSeconds and the value is 1.
 
 // f) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+// When pad is called for the last time, the assigned value is 1 and the return value is 01.
+// To explain further, 01 is returned because pad has been defined to return exactly two characters such that if
+// the value passed is already two digits, the 2 digits are returned. If one digit is passed, 0 is added to the left-side.
