3fd37c9e9f2178a7b7b602c00e5b38476e459fcc
berwin eibo <perween.kurd1@gmail.com>
2023-10-05 23:15:21 +0100

    0.js done

--- week-2/debug/0.js
+++ week-2/debug/0.js
@@ -8,3 +8,4 @@ console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // the function multiply logs the result of the multiplication of a and b to the console but does not return any Value.
 // console.log: to output the result of 10*32 by using the multiply function.
+//  it returns undefined.
