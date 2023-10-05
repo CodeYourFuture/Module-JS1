17c0a6d924e2ce521d9c0322e9a12f9d185fa699
berwin eibo <perween.kurd1@gmail.com>
2023-10-05 23:38:34 +0100

    1.js done

--- week-2/debug/1.js
+++ week-2/debug/1.js
@@ -6,3 +6,6 @@ function sum(a, b) {
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+//the sum of 10 and 32 will return undefined. there is a sum function that adds a and b but does not return the sum because it doesnot actually sum them as return prevents the sum to reach a and b
+
+//the return statement is empty and followed by any value
