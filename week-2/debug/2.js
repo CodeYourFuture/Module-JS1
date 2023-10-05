1c8c20b86c674cbb7394d070308cfce814100639
aishaathmanlali <aishaathmanal@gmail.com>
2023-10-05 14:05:44 +0100

    add answer to debug/2.js

--- week-2/debug/2.js
+++ week-2/debug/2.js
@@ -1,14 +1,16 @@
 // Predict and explain first...
 
-const num = 103;
+// const num = 103;
 
-function getLastDigit() {
-  return num.toString().slice(-1);
+function getLastDigit(number) {
+  return number.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
+
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+// It is not working properly because const num variable was declared as 103 and its a global variable. Therefore everytime the function is called it goes to the beginning and gets the variable num as 103.
