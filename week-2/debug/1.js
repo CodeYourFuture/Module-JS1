f23f5be39cf225cd04c15c7ce20e7e6a70ca0bea
Zahra-Khademi <zahrakhademi420@gmail.com>
2023-10-05 08:36:51 +0100

    Debugging1

--- week-2/debug/1.js
+++ week-2/debug/1.js
@@ -1,8 +1,9 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+//In the sum function,  a return statement had no value. We should give the value of a + b to the return to get the result of the addition.
