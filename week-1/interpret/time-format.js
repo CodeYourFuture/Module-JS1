33c41820f2ab10519eea78993fca7e0d97b19fc7
bahador <noor.bahadory786@gmail.com>
2023-10-05 01:26:50 +0100

    started to-pounds.js

--- week-1/interpret/time-format.js
+++ week-1/interpret/time-format.js
@@ -1,11 +1,10 @@
-const movieLength = 8784; // length of movie in seconds
+const movieLength = 12998898; // length of movie in seconds
 
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
 
 const remainingMinutes = totalMinutes % 60;
 const totalHours = (totalMinutes - remainingMinutes) / 60;
-
 const remainingHours = totalHours % 24;
 
 const result = `${remainingHours}:${remainingMinutes}:${remainingSeconds}`;
@@ -14,13 +13,16 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
+// a answer) there are 7 variable declaration in this program.
 
 // b) How many function calls are there?
+// b answer) 0
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+// e answer)
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
