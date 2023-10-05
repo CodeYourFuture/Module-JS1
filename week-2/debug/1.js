206f7c7f3ad329b81b9b00246991ebacefbf6672
aishaathmanlali <aishaathmanal@gmail.com>
2023-10-05 14:11:13 +0100

    edit answer to debug/1.js

--- week-2/debug/1.js
+++ week-2/debug/1.js
@@ -1,12 +1,9 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-/*
-This function will not work because there is a semi colon after the return which should not be there.
-Also a+b should be inside brackets like this (a+b) and a semi colon at the end.
+// This function will not work because there is a semi colon after the return which should not be there. 
