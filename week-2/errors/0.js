322103c1a28a266bf95acc5710cf1bb59ec1a9c8
aishaathmanlali <aishaathmanal@gmail.com>
2023-10-05 15:11:22 +0100

    errors/0.js

--- week-2/errors/0.js
+++ week-2/errors/0.js
@@ -4,6 +4,9 @@
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+
 }
+
+// This function will not work because the variable string has been declared twice. First in the parameters of the function and second in line 7. Therefore I have removed let str and replaced it with return. Now the code should work.
+console.log(capitalise("aisha"));
