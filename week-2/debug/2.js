5c1a611f682f6f87516596d972aae9d85e76ebfc
Zahra-Khademi <zahrakhademi420@gmail.com>
2023-10-06 17:26:52 +0100

    debugging2

--- week-2/debug/2.js
+++ week-2/debug/2.js
@@ -1,9 +1,8 @@
 // Predict and explain first...
 
-const num = 103;
 
-function getLastDigit() {
-  return num.toString().slice(-1);
+function getLastDigit(number) {
+  return number.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
@@ -12,3 +11,5 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+
+//We need to add a parameter number to the getLastDigit function to accept the number whose last digit we want to find.
