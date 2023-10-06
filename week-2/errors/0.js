1740d2c6f008e63f84211b0186e2248e9a92b732
elmira <elmira.zangeneh89@gmail.com>
2023-10-06 19:43:54 +0100

    comlete js errors

--- week-2/errors/0.js
+++ week-2/errors/0.js
@@ -4,6 +4,10 @@
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let capitalisestr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalisestr;
 }
+console.log(capitalise("hello")); // Output: "Hello"
+
+//My Explanation:there is a problem with variable naming.You have defined a function called capitalise that takes a str parameter, but inside the function, you are also trying to declare a variable with the same name str. This will result in a syntax error because you cannot declare two variables with the same name in the same scope.
+//In this corrected code, I changed the variable name from str to capitalizedStr inside the capitalise function to avoid the conflict, and now the code should work without any syntax errors.
