b69a4ce8bd7cc43cc94bd9de32cdae40a474fa26
Zahra-Khademi <zahrakhademi420@gmail.com>
2023-10-06 18:56:33 +0100

    fixing the error 2

--- week-2/errors/2.js
+++ week-2/errors/2.js
@@ -3,8 +3,8 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+function square(num) {
     return num * num;
 }
 
-
+//This is an invalid JavaScript parameter definition. Function declarations should only include identifiers (variable names) as parameters rather than particular values or expressions.
