dc63947be705302c10f702097ec9169b3a9741f0
Zahra-Khademi <zahrakhademi420@gmail.com>
2023-10-06 18:03:45 +0100

    fixing the error0

--- week-2/errors/0.js
+++ week-2/errors/0.js
@@ -4,6 +4,9 @@
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let capitalizedStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalizedStr;
 }
+
+//The variable str is already declared as the function parameter. You cannot declare a new variable with the same name within the same scope.
+// The variable name inside the function should be changed.
