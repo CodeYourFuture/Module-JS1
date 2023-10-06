c451bb4d15747ef10ef1bc47c50c500168976115
elmira <elmira.zangeneh89@gmail.com>
2023-10-06 19:56:00 +0100

    complete errors

--- week-2/errors/2.js
+++ week-2/errors/2.js
@@ -3,8 +3,13 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+function square(num) {
     return num * num;
 }
+console.log(square(3)); // Output: 9
+
+//The function name should be "square," but you've used the number "3" as the function parameter. Function parameters should be variable names, not specific values.
+//You're attempting to square a variable called num, but you haven't defined or declared num anywhere in the function or as a parameter.
+
 
 
