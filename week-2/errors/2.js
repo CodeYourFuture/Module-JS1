f77d92e13cdc8b393f68e9c1340e158d2196259f
karimi <behrouzkarimi1993@gmail.com>
2023-10-07 15:02:52 +0100

    2.js week-2 , errors section

--- week-2/errors/2.js
+++ week-2/errors/2.js
@@ -1,10 +1,22 @@
 
 // Predict and explain first...
+// this function `square` tries to square the passed parameter , but in JS variable naming rules ,
+// we're not allowed to start a variable name by numbers or just a number for naming variable .
+//another problem is in the function's body `return num*num ;` , num is has not been declared .
+
+
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
-    return num * num;
+//////////////////////////////////////////////////////////////////////////////////////
+// function square(3) {
+//     return num * num;
+// }
+////////////////////////////////////////////////////////////////////////////////////
+
+
+function squareCorrectedVersion(num){
+    return num*num;
 }
 
-
+console.log(squareCorrectedVersion(2));
