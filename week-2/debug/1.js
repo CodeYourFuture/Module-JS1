ee43ff4e7062af31b995081caaed27e5c1767ded
karimi <behrouzkarimi1993@gmail.com>
2023-10-07 13:48:48 +0100

    1.js week-2 debug

--- week-2/debug/1.js
+++ week-2/debug/1.js
@@ -6,3 +6,13 @@ function sum(a, b) {
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// in the function definition part lines 3 to 6 ; return has been written by its own and it doesn't return anything 
+// actually its return is undefined , and the line after the a+b has been written ,
+//the corrected version of function is like what you cansee below :
+
+function sumCorrectedVersion(a,b){
+  return a+b;
+}
+
+console.log(`what corrected version of sun will return is ${sumCorrectedVersion(10, 32)}`);
