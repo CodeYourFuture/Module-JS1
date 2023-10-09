8956279d27fec0ce4bb9f6b8bb793cb9eeae1cb0
karam ali <karam780@live.co.uk>
2023-10-09 11:51:28 +0100

    completed errors

--- week-2/errors/1.js
+++ week-2/errors/1.js
@@ -3,11 +3,26 @@
 // Why will an error occur when this program runs?
 // Try playing computer with the example to work out what is going on
 
+//This will not work because in line 7 the variable decimal number declaration came after the function. It should come first then the function declaration.
+//Also we need to call the function in the console log for it to work.
+
+// function convertToPercentage(decimalNumber) {
+//   const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
+
+//   return percentage;
+// }
+
+// console.log(decimalNumber);
+
+//I have corrected the code below
+
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+ 
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+
+console.log(convertToPercentage(0.7));
