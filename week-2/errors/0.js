0671a8ee26e1cdedbe21cb9850c7253ff2dbab8f
berwin eibo <perween.kurd1@gmail.com>
2023-10-10 20:51:29 +0100

    errors/1.js solved

--- week-2/errors/0.js
+++ week-2/errors/0.js
@@ -2,10 +2,11 @@
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+// an error occured because we are trying to create a variable with the same name used for the function parameter str in the same scope. this causes conflict and to fix this mistake a different name should be used.
+
 
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
 
-an error occured because we are trying to create a variable with the same name used for the function parameter str in the same scope. this causes conflict and to fix this mistake a different name should be used.
--- week-2/errors/1.js
+++ week-2/errors/1.js
@@ -1,7 +1,8 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// Try playing computer with the example to work out what is going on
+// Try playing computer with the example to work out what is going on.
+// we have a function with a parameter decimalNumber and within this function we are trying to declare a new variable with the same name of the parameter e.g. const decimalNumber however we cannot use the same name within the scope. to fix this we can remove const decimalNumber=0.5; and in the console.log write the function with 0.5 as a parameter e.g. console.log(convertToPercentage(0.5));
 
 function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
@@ -11,3 +12,4 @@ function convertToPercentage(decimalNumber) {
 }
 
 console.log(decimalNumber);
+
