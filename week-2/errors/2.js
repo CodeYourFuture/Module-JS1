e4d0aad0e456f8a08af8467ad07026f124f8f4e2
berwin eibo <perween.kurd1@gmail.com>
2023-10-10 21:40:59 +0100

    errors/2.js solved

--- week-2/errors/2.js
+++ week-2/errors/2.js
@@ -2,9 +2,7 @@
 // Predict and explain first...
 
 // this function should square any number but instead we're going to get an error
-
+// because in return we ask num * num but js does not know num as it is not defined in any declaration/variable. also we have 3 as a parameter which is not correct and we need instead to replace it with num so that js recognise it when we return it and in this way we can pass any number into the argument. then we call using console.log(square(3));
 function square(3) {
     return num * num;
 }
-
-
