bea9cda6fa8624b9e8cccf42de4fb3c1f0830b3e
elmira <elmira.zangeneh89@gmail.com>
2023-10-11 13:19:52 +0100

    debug format-as-12-hours

--- week-3/debug/format-as-12-hours.js
+++ week-3/debug/format-as-12-hours.js
@@ -26,5 +26,13 @@ console.assert(
 // formatAs12HourClock currently has a 🐛
 
 // a) Write an assertion to check the return value of formatAs12HourClock when it is called with an input "17:42"
+const result = formatAs12HourClock("17:42");
+console.log(result); // Output should be "5:42 pm"
+//The assert() method tests if a given expression is true or not.
+
 // b) Check the assertion output and explain what the bug is
+
+//To fix this bug, you should modify the code to check if the hours are greater than or equal to 12 (12 or greater) instead of just greater than 12. This change ensures that "12:00" is correctly interpreted as noon.
+// With this fix, the function will now produce the expected output of "12:00 pm" when the input is "12:00."
+
 // c) Now fix the bug and re-run all your assertions
