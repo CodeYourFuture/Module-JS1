e0f8e0ab14f946382ba77382fd531ad6e6455678
aishaathmanlali <aishaathmanal@gmail.com>
2023-10-11 22:32:52 +0100

    add assertions get-angle-type

--- week-3/implement/get-angle-type.js
+++ week-3/implement/get-angle-type.js
@@ -29,19 +29,65 @@
 
 function getAngleType(number) {
     if(number===90){
-        return `Right angle`;
+        return "Right angle";
     }
     else if(number<90){
-        return `Acute angle`;
+        return "Acute angle";
     }
     else if(number > 90 && number < 180){
-        return `Obtuse angle`;
+        return "Obtuse angle";
     }
     else if(number===180){
-        return `Straight angle`;
+        return "Straight angle";
     }
     else if(number > 180 && number < 360){
-        return `Reflex angle`;
+        return "Reflex angle";
     }
 
 }
+
+const currentOutput = getAngleType(38);
+const targetOutput = "Acute angle";
+console.assert(
+  currentOutput === targetOutput,
+  "current output: %s, target output: %s",
+  currentOutput,
+  targetOutput
+);
+
+const currentOutput2 = getAngleType(90);
+const targetOutput2 = "Right angle";
+console.assert(
+  currentOutput2 === targetOutput2,
+  "current output: %s, target output: %s",
+  currentOutput2,
+  targetOutput2
+);
+
+const currentOutput3 = getAngleType(260);
+const targetOutput3 = "Reflex angle";
+console.assert(
+  currentOutput3 === targetOutput3,
+  "current output: %s, target output: %s",
+  currentOutput3,
+  targetOutput3
+);
+
+const currentOutput4 = getAngleType(180);
+const targetOutput4 = "Straight angle";
+console.assert(
+  currentOutput4 === targetOutput4,
+  "current output: %s, target output: %s",
+  currentOutput4,
+  targetOutput4
+);
+
+const currentOutput5 = getAngleType(143);
+const targetOutput5 = "Obtuse angle";
+console.assert(
+  currentOutput5 === targetOutput5,
+  "current output: %s, target output: %s",
+  currentOutput5,
+  targetOutput5
+);
+
