26365168589b03c44b737f3ed14a0f053cc72738
bahador <noor.bahadory786@gmail.com>
2023-10-13 00:17:43 +0100

    completed interpret

--- week-2/implement/vat.js
+++ week-2/implement/vat.js
@@ -9,22 +9,12 @@
 // When I call this function with a number
 // it returns the new price with VAT added on
 
-function includesVAT(items, price, vat) {
-  //   const vatCalculation = price * 1.2;
-  if (vat === true) {
-    return `Your ${items} has vat. The price is:  price * 1.2`;
-  } else {
-    return price;
-  }
+function includesVAT(items) {
+  let vat = items * 1.2;
+  return `${vat}£`;
 }
+let vatResult = includesVAT(200);
+console.log(vatResult);
 
-console.log(includesVat(100));
-
-// 1 = Provide items
-// Option 1 = Clothing (Does have VAT) = £200 * 1.2 =  £240
-// Option 2 = Cat items (Doesn't have VAT) = £45
-// Option 3 = Shoes (Doesn't have VAT) = £120
-// Option 4 = Garden equipment (Does have VAT) = £844 * 1.2 = £1012.80
-
-// 2 = Does the product have 20% VAT?
-// Step 1 = if statement for if true or not.
+vatResult = includesVAT(100);
+console.log(vatResult);
