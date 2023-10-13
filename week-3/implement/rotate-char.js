0e69d30a77cc7d3165dfc67d6d0398123d63b995
aishaathmanlali <aishaathmanal@gmail.com>
2023-10-14 00:16:58 +0100

    add answer rotate-char.js

--- week-3/implement/rotate-char.js
+++ week-3/implement/rotate-char.js
@@ -1,3 +1,21 @@
+const alphabetLowerCase = [..."abcdefghijklmnopqrstuvwxyz"];
+const alphabetUpperCase = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+function rotateCharacter(letter, shift) {
+  if (alphabetLowerCase.indexOf(letter) + shift >= 26) {
+    return alphabetLowerCase[alphabetLowerCase.indexOf(letter) + shift - 26];
+  } else if (alphabetUpperCase.indexOf(letter) + shift >= 26) {
+    return alphabetUpperCase[alphabetUpperCase.indexOf(letter) + shift - 26];
+  } else if (alphabetUpperCase.includes(letter)) {
+    return alphabetUpperCase[alphabetUpperCase.indexOf(letter) + shift];
+  } else if (alphabetLowerCase.includes(letter)) {
+    return alphabetLowerCase[alphabetLowerCase.indexOf(letter) + shift];
+  } else if (letter != alphabetLowerCase || letter != alphabetUpperCase) {
+    return letter;
+  }
+}
+
+
 // The rotateCharacter function takes a character and a shift value as input.
 // If the character is a letter (either uppercase or lowercase),
 // it rotates the character by the specified shift value within the alphabet,
@@ -17,7 +35,7 @@
 // When the function is called with these inputs,
 // Then it should rotate the lowercase letter by shift positions within the lowercase alphabet, wrapping around if necessary, and return the rotated lowercase letter as a string.
 console.log(rotateCharacter("a", 3)); // Output: "d"
-console.log(rotateCharacter("f", 1)); // Output: "f"
+console.log(rotateCharacter("f", 1)); // Output: "g"
 
 // Scenario: Rotate Uppercase Letters:
 // Given an uppercase letter character (char) and a positive integer shift,
@@ -40,3 +58,6 @@ console.log(rotateCharacter("7", 5)); // Output: "7" (unchanged, not a letter)
 // Then it should correctly rotate the character by shift positions within the alphabet while handling the wraparound,
 // And the function should return the rotated character as a string (e.g., 'z' rotated by 3 should become 'c', 'Z' rotated by 3 should become 'C').
 console.log(rotateCharacter("z", 1)); // Output: "a" (unchanged, not a letter)
+
+console.log(rotateCharacter("x", 5)); // Output: "a" (unchanged, not a letter)
+
