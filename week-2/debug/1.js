6bd66621202be17f7397e0e1cd4b222d8026b411
aishaathmanlali <aishaathmanal@gmail.com>
2023-10-13 14:52:10 +0100

    edit answer 1.js

--- week-2/debug/1.js
+++ week-2/debug/1.js
@@ -2,8 +2,9 @@
 
 function sum(a, b) {
   return a + b;
+  
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// This function will not work because there is a semi colon after the return which should not be there. 
+// This function will not work because there is a semi colon after the return which should not be there. With the semi-colon immediately after the return it will return undefined as the answer. 
