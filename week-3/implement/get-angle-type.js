e843fba971eb304830b8059f765a469d8f2faf60
elmira <elmira.zangeneh89@gmail.com>
2023-10-14 09:00:51 +0100

    complete the angle type

--- week-3/implement/get-angle-type.js
+++ week-3/implement/get-angle-type.js
@@ -7,21 +7,38 @@
 // Then it should:
 
 // Identify Right Angles:
-// When the angle is exactly 90 degrees,
+// When the angle is exactly 90 degrees,--> angle===90
 // Then the function should return "Right angle"
 
 // Identify Acute Angles:
-// When the angle is less than 90 degrees,
+// When the angle is less than 90 degrees,--> angle < 90
 // Then the function should return "Acute angle"
 
 // Identify Obtuse Angles:
-// When the angle is greater than 90 degrees and less than 180 degrees,
+// When the angle is greater than 90 degrees and less than 180 degrees,--> angle > 90
 // Then the function should return "Obtuse angle"
 
 // Identify Straight Angles:
-// When the angle is exactly 180 degrees,
+// When the angle is exactly 180 degrees,--> angle === 180
 // Then the function should return "Straight angle"
 
 // Identify Reflex Angles:
-// When the angle is greater than 180 degrees and less than 360 degrees,
+// When the angle is greater than 180 degrees and less than 360 degrees,--> angle === 360
 // Then the function should return "Reflex angle"
+function getAngleType(angle){
+        if (angle === 90) {
+        return "Right angle";
+    } else if (angle < 90) {
+        return "Acute angle";
+    } else if (angle > 90 && angle < 180) {
+        return "Obtuse angle";
+    } else if (angle === 180) {
+        return "Straight angle";
+    } else if (angle > 180 && angle < 360) {
+        return "Reflex angle";
+    } else {
+        return "Invalid angle. Please enter an angle between 0 and 360 degrees.";
+    }
+}
+const angle = 100;
+console.log(getAngleType(angle)); // Should output "Obtuse angle"
