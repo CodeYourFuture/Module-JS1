8a138c2a9d80664caff558bdb62f6845b9288a76
berwin eibo <perween.kurd1@gmail.com>
2023-10-14 14:26:58 +0100

    interpret/time-format

--- week-2/interpret/time-format.js
+++ week-2/interpret/time-format.js
@@ -21,16 +21,17 @@ console.log(formatTimeDisplay(61));
 
 // Questions
 
-// a) Run this program with node, identify the line the error is thrown. Explain why this error is occurring. How can you fix this error?
+// a) Run this program with node, identify the line the error is thrown. Explain why this error is occurring. How can you fix this error? line 10 because total hours is calculated based on the number of minutes in second and not based on 24 hours.can be fixed by const totalHours = Math.floor(totalMinutes / 60) % 24;
+
 
 // b) When formatTimeDisplay is called how many times will pad be called? 3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
-// c) What is the value assigned to num when pad is called for the first time?
+// c) What is the value assigned to num when pad is called for the first time? 0
 
-// d) What is the return value of pad is called for the first time?
+// d) What is the return value of pad is called for the first time? 00
 
-// e) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
+// e) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer. 1, because is what is left from the seconds after subtracting complete minutes from seconds.
 
-// f) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+// f) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer. 01 ,  because padStart(2, "0") converts the number 1 to a string and pads it with a leading '0'.
