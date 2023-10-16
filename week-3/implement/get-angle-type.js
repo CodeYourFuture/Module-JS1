90626ee565eac4762d1531a1556363ec05227038
karimi <behrouzkarimi1993@gmail.com>
2023-10-16 01:41:25 +0100

    get-angel-js1-week3-exercise

--- week-3/implement/get-angle-type.js
+++ week-3/implement/get-angle-type.js
@@ -25,3 +25,46 @@
 // Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
+
+function getAngleType(angel){
+
+    if(typeof angel=='number'){
+        if(angel===90) return 'Right angel';
+
+        else if(angel<90 && angel>=0) return 'Acute angel';
+
+        else if(angel>90 && angel<180) return 'Obtuse angel' ;
+
+        else if(angel===180) return 'Straight angel' ;
+
+        else if (angel>180 && angel<360) return 'Reflex angel' ;
+
+        else if(angel===360) return 'Full angel';  // it hasn't been menthioned if angel is 360 degree ; which is a full angel.
+
+        else return `Please Enter the degree's value in range of [0,360] (Including)`;
+
+        
+    }
+
+    else return 'Enter a valid value'
+}
+    
+
+
+        
+
+    
+
+
+console.assert(getAngleType(90)==='Right angel');
+console.assert(getAngleType(76)==='Acute angel');
+
+console.log(getAngleType(123));
+console.log(getAngleType(0));
+console.log(getAngleType(90));
+console.log(getAngleType(180));
+console.log(getAngleType(270));
+console.log(getAngleType(720));
+console.log(getAngleType(true));
+console.log(getAngleType('t'));
+console.log(getAngleType('9'));
