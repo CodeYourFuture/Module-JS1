f461b4d4c938d3c72913c0a472ebcc16c0ef739c
karimi <behrouzkarimi1993@gmail.com>
2023-10-16 12:30:27 +0100

    is-valid-traingle-js1-week3-exercise

--- week-3/implement/is-valid-triangle.js
+++ week-3/implement/is-valid-triangle.js
@@ -1,4 +1,4 @@
-// STRETCH Implement a function isValidTriangle
+// STRETCH Implement a function iisValidTriangle
 // Terms
 // the Triangle Inequality says: the sum of any two sides is always greater than the third side.
 // practical examples:
@@ -11,13 +11,13 @@
 // It's also true that b + c > a
 // It's also true that a + c > b
 
-// In our function isValidTriangle, we need to invalidate any triangle where the sum of any two sides is less than or equal to the length of the third side.
+// In our function iisValidTriangle, we need to invalidate any triangle where the sum of any two sides is less than or equal to the length of the third side.
 // and we need to validate any triangle where the sum of any two sides is greater than the length of the third side.
 
 // Acceptance criteria:
 
 // Given the lengths of three sides of a triangle (a, b, c),
-// When the function isValidTriangle is called with these side lengths as input,
+// When the function iisValidTriangle is called with these side lengths as input,
 // Then it should:
 
 // scenario: invalid triangle
@@ -36,4 +36,37 @@
 // When the function is called with these values as input,
 // Then it should return true because the input forms a valid triangle.
 
-// This specification outlines the behavior of the isValidTriangle function for different input scenarios, ensuring it properly checks for invalid side lengths and whether they form a valid triangle according to the Triangle Inequality Theorem.
+// This specification outlines the behavior of the iisValidTriangle function for different input scenarios, ensuring it properly checks for invalid side lengths and whether they form a valid triangle according to the Triangle Inequality Theorem.
+
+function isValidTriangle(a,b,c){
+    
+    if((a+b>c && a+c>b && b+c>a) &&
+    (a>0 && b>0 && c>0) &&
+    (typeof a ==='number' && typeof b==='number' && typeof c==='number')){
+        if(a!==b && a!==c && b!==c){
+            return 'its a scalene traingle' ;
+            
+        }
+        else if(a===b && a===c && b===c){
+            return 'its a equialterial taringle' ;
+        }
+        else if((a===b && a!==c )||(b===c && b!==a) || (a===c && a!==b)){
+            return 'its isosecle traingle';
+        }
+    }
+    else {
+        return 'a,b,c are not valid values to create a traingle';
+    }
+}
+
+// this function beside checking for valid value's to create a traingle based on the theory it also indicate
+// which type of triangle the vlue's make 
+
+console.assert(isValidTriangle(4,4,4)=== 'its a equialterial taringle');
+console.assert(isValidTriangle(3,3,5)=== 'its isosecle traingle');
+console.assert(isValidTriangle(3,5,3)=== 'its isosecle traingle');
+console.assert(isValidTriangle(5,3,3)=== 'its isosecle traingle');
+console.assert(isValidTriangle(3,4,5)=== 'its a scalene traingle');
+console.assert(isValidTriangle(3,'4',5)=== 'a,b,c are not valid values to create a traingle');
+console.assert(isValidTriangle(3,-4,5)=== 'a,b,c are not valid values to create a traingle');
+console.assert(isValidTriangle(3,-4,0)=== 'a,b,c are not valid values to create a traingle');
