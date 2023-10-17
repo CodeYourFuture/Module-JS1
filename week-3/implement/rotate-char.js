0e6c7e70062f11d2f60435caebbf422eb10ce027
karimi <behrouzkarimi1993@gmail.com>
2023-10-17 03:04:08 +0100

    rotate-char-js1-week3-exercise

--- week-3/implement/rotate-char.js
+++ week-3/implement/rotate-char.js
@@ -17,7 +17,7 @@
 // When the function is called with these inputs,
 // Then it should rotate the lowercase letter by shift positions within the lowercase alphabet, wrapping around if necessary, and return the rotated lowercase letter as a string.
 console.log(rotateCharacter("a", 3)); // Output: "d"
-console.log(rotateCharacter("f", 1)); // Output: "f"
+console.log(rotateCharacter("f", 1)); // Output: "f"  
 
 // Scenario: Rotate Uppercase Letters:
 // Given an uppercase letter character (char) and a positive integer shift,
@@ -40,3 +40,53 @@ console.log(rotateCharacter("7", 5)); // Output: "7" (unchanged, not a letter)
 // Then it should correctly rotate the character by shift positions within the alphabet while handling the wraparound,
 // And the function should return the rotated character as a string (e.g., 'z' rotated by 3 should become 'c', 'Z' rotated by 3 should become 'C').
 console.log(rotateCharacter("z", 1)); // Output: "a" (unchanged, not a letter)
+
+
+function rotateCharacter(char,shift){
+
+    const arrayRange = (start, stop, step) =>
+        Array.from(
+        { length: (stop - start) / step + 1 },
+        (value, index) => start + index * step
+        );
+
+    const upperCaseAsci=arrayRange(65,90,1);
+    const lowerCaseAsci=arrayRange(97,122,1);
+    
+    const newCharArray=[];
+
+    if(typeof char==='string'){
+        const charArray=char.split('');
+        
+        
+        for(let i=0;i<charArray.length;i++){
+            if(upperCaseAsci.includes(charArray[i].charCodeAt())){
+                let index=upperCaseAsci.indexOf(charArray[i].charCodeAt());
+                let newIndex=(index+shift)%26;
+                newCharArray.push(String.fromCharCode(upperCaseAsci[newIndex]));
+            }
+
+
+            else if(lowerCaseAsci.includes(charArray[i].charCodeAt())){
+                let index=lowerCaseAsci.indexOf(charArray[i].charCodeAt());
+                let newIndex=(index+shift)%26 ;
+                newCharArray.push(String.fromCharCode(lowerCaseAsci[newIndex]));
+            }
+            else{
+                newCharArray.push(charArray[i]);
+            }
+
+        }
+
+    }
+    else
+        return 'first parametre must a string!! enter a valid value pls .' ;
+
+
+    let newChar=newCharArray.join('');
+    return newChar;
+}
+
+console.log(rotateCharacter('a',3)==='d');
+console.log(rotateCharacter('ABC',1));
+console.log(rotateCharacter(true,1));
