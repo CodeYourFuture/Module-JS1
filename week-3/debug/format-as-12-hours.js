d5fb40054eb786455d36ae2d29ee03da14956511
Tajudeen Oyehan <oyehan.uob@gmail.com>
2023-10-18 21:57:08 +0100

    bug in format-as-12-hours.js fixed

--- week-3/debug/format-as-12-hours.js
+++ week-3/debug/format-as-12-hours.js
@@ -1,6 +1,6 @@
 function formatAs12HourClock(time) {
   if (Number(time.slice(0, 2)) > 12) {
-    return `${Number(time.slice(0, 2)) - 12}:00 pm`;
+    return `${Number(time.slice(0, 2)) - 12}:${time.slice(3)} pm`;
   }
   return `${time} am`;
 }
@@ -24,7 +24,18 @@ console.assert(
 );
 
 // formatAs12HourClock currently has a 🐛
-
 // a) Write an assertion to check the return value of formatAs12HourClock when it is called with an input "17:42"
+const currentOutput3 = formatAs12HourClock("17:52");
+const targetOutput3 = "5:52 pm";
+console.assert(
+  currentOutput3 === targetOutput3,
+  "current output: %s, target output: %s",
+  currentOutput3,
+  targetOutput3
+);
+
 // b) Check the assertion output and explain what the bug is
+// The bug has to do with hard-coding the minutes as "00", which implies that minutes other than 00 will not be correctly returned.
+
 // c) Now fix the bug and re-run all your assertions
+// To fix this, use slice() or subString() method to return the same minutes as in the time - args.
