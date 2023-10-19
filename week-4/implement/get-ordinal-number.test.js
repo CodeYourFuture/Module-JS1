167706e1ebe7b41d3afe375edc553a5470da3744
karimi <behrouzkarimi1993@gmail.com>
2023-10-19 13:00:39 +0100

    ordinalNumbers-JS1-week4-Exercises

--- week-4/implement/get-ordinal-number.test.js
+++ week-4/implement/get-ordinal-number.test.js
@@ -2,3 +2,37 @@
 
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
+
+function getOrdinalNumber(number){
+    if(typeof number==='number' 
+    && number>0 && 
+    Number.isInteger(number))
+    {
+        let stringedNumber=number.toString();
+        if(stringedNumber[stringedNumber.length-1]==='1' && number!=11){
+            return stringedNumber+'st';
+        }
+        else if(stringedNumber[stringedNumber.length-1]==='2' && number!=12){
+            return stringedNumber+'nd';
+        }
+        else if(stringedNumber[stringedNumber.length-1]==='3' && number!=13){
+            return stringedNumber+'rd';
+        }
+        else return stringedNumber+'th';
+    }
+
+    else return 'Enter a valid positive integer number '
+    
+    
+
+}
+console.log(getOrdinalNumber(14));
+console.log(getOrdinalNumber(11));
+console.log(getOrdinalNumber(10));
+console.log(getOrdinalNumber(1));
+console.log(getOrdinalNumber(122));
+console.log(getOrdinalNumber(33));
+console.log(getOrdinalNumber(3));
+console.log(getOrdinalNumber('h'));
+console.log(getOrdinalNumber(false));
+console.log(getOrdinalNumber('3'));
