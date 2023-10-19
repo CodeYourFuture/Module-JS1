9d193bee195b7b3b7d8b35ed928ccbe9bf2dfd4a
karam ali <karam780@live.co.uk>
2023-10-19 10:20:06 +0100

    completed refactor exercises

--- week-3/refactor/format-as-12-hours.js
+++ week-3/refactor/format-as-12-hours.js
@@ -2,4 +2,14 @@
 // This implementation currently uses the expression Number(time.slice(0, 2)) twice
 // Store this expression in a variable and reference it twice in the function in the correct place
 
+function formatAs12HourClock(time) {
+  if (Number(time.slice(0, 2)) > 12) {
+    const HoursInput = Number(time.slice(0, 2));
+    const MinutesInput = Number(time.slice(3, 5));
+    return `${HoursInput - 12}:${MinutesInput} pm`;
+  }
+  return `${time} am`;
+}
+
 // Explain why it makes more sense to store this expression in a variable
+// This function is more readable and this Number(time.slice(3, 5)) code is reusable.
