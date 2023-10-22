0cedb3ed96a430bc780392c7e628b825b1a8d5a0
berwin eibo <perween.kurd1@gmail.com>
2023-10-23 00:00:18 +0100

    format as 12 hours done

--- week-3/refactor/format-as-12-hours.js
+++ week-3/refactor/format-as-12-hours.js
@@ -3,3 +3,15 @@
 // Store this expression in a variable and reference it twice in the function in the correct place
 
 // Explain why it makes more sense to store this expression in a variable
+
+function formatAs12HourClock(time) {
+  const hours = Number(time.slice(0, 2)); 
+
+  if (hours > 12) {
+    return `${hours - 12}:00 pm`;
+  }
+  return `${time} am`;
+}
+const formattedTime = formatAs12HourClock("22:00"); 
+console.log(formattedTime); 
+
