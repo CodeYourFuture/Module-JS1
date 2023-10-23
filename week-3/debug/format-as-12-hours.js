e684c1f433e8efec4d411ac8d7d8a67b33788ec0
LauraSantiag0 <eusoulaura8@gmail.com>
2023-10-23 16:27:10 +0100

    debug

--- week-3/debug/format-as-12-hours.js
+++ week-3/debug/format-as-12-hours.js
@@ -1,3 +1,4 @@
+/*
 function formatAs12HourClock(time) {
   if (Number(time.slice(0, 2)) > 12) {
     return `${Number(time.slice(0, 2)) - 12}:00 pm`;
@@ -27,4 +28,43 @@ console.assert(
 
 // a) Write an assertion to check the return value of formatAs12HourClock when it is called with an input "17:42"
 // b) Check the assertion output and explain what the bug is
-// c) Now fix the bug and re-run all your assertions
+// c) Now fix the bug and re-run all your assertions */
+
+
+function formatAs12HourClock(time) {
+  const hours = Number(time.slice(0, 2));
+  if (hours >= 12) {
+    if (hours > 12) {
+      return `${hours - 12}:${time.slice(3)} pm`;
+    }
+    return `${time} pm`;
+  }
+  return `${time} am`;
+}
+
+const currentOutput = formatAs12HourClock("08:00");
+const targetOutput = "08:00 am";
+console.assert(
+  currentOutput === targetOutput,
+  "current output: %s, expected output: %s",
+  currentOutput,
+  targetOutput
+);
+
+const currentOutput2 = formatAs12HourClock("23:00");
+const targetOutput2 = "11:00 pm";
+console.assert(
+  currentOutput2 === targetOutput2,
+  "current output: %s, expected output: %s",
+  currentOutput2,
+  targetOutput2
+);
+
+const currentOutput3 = formatAs12HourClock("17:42");
+const targetOutput3 = "5:42 pm";
+console.assert(
+  currentOutput3 === targetOutput3,
+  "current output: %s, expected output: %s",
+  currentOutput3,
+  targetOutput3
+);
