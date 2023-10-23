825fa1154f582c151afe227b8519fa0d259266cd
aishaathmanlali <aishaathmanal@gmail.com>
2023-10-23 16:41:22 +0100

    add answers find.js

--- week-4/investigate/find.js
+++ week-4/investigate/find.js
@@ -11,6 +11,7 @@ function find(str, char) {
 }
 
 console.log(find("code your future", "u"));
+
 console.log(find("code your future", "z"));
 
 // The while loop statement allows us to do iteration - the repetition of a certain number of tasks according to some condition
@@ -20,6 +21,10 @@ console.log(find("code your future", "z"));
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
+// the index keeps on increasing by 1
 // b) What is the if statement used to check
+// It is checking if the given character is available in the string and the index that the character is found
 // c) Why is index++ being used?
+// because its a loop and the condition goes to check on the next index everytime it returns false
 // d) What is the condition index < str.length used for?
+// It is used to check all the characters using their index in a string
