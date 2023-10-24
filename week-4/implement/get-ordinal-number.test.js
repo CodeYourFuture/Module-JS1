179eccc09a75f8f46cf610c6659fa8ed231c64c0
Sariat <adenike.sariat@gmail.com>
2023-10-24 14:38:16 +0100

    writing test for the function

--- week-4/implement/get-ordinal-number.test.js
+++ week-4/implement/get-ordinal-number.test.js
@@ -2,3 +2,11 @@
 
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
+
+
+test("works for any number ending in 1", () => {
+  expect(getOrdinalNumber(1)).toBe("1st");
+  expect(getOrdinalNumber(11)).toBe("11th");
+  expect(getOrdinalNumber(32)).toBe("32nd");
+  expect(getOrdinalNumber(43)).toBe("43rd");
+});
