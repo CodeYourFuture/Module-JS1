48b193fe50cd957cd2756f9cf56565c672eee908
karimi <behrouzkarimi1993@gmail.com>
2023-10-24 12:13:51 +0100

    isPrime week-4-exercise

--- week-4/implement/is-prime.test.js
+++ week-4/implement/is-prime.test.js
@@ -2,18 +2,18 @@
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
 
-function isPrime(number){
-    if( typeof number==='number' && number>1){
-        for(let i=2;i<=Math.floor(number/2);i++){
-            if(number%i===0){
-                return `${number} is not a prime number`;
+function isPrime(num){
+    if( typeof num==='number' && num>1 && Number.isInteger(num)){
+        for(let i=2;i<=Math.floor(num/2);i++){
+            if(num%i===0){
+                return `${num} is not a prime num`;
             }
         }
         
-    return `${number} is prime`
+        return `${num} is prime`
     }
-    else if(typeof number==='number'&& number===1) {
-        return `${1} is not a prime number` ;
+    else if(typeof num==='number'&& num===1) {
+        return `${1} is not a prime num` ;
     }
 
     else return 'enter a valid value' ;
@@ -23,7 +23,7 @@ function isPrime(number){
 }
 
 // there are some ways that code could to be optimized a bit more in execution time , like if the last digit is even or 5 
-//the number is not prime or if the sum of the digit is divideable to 3 it's not prime as well. it's useful for larg numbers;
+//the num is not prime or if the sum of the digit is divideable to 3 it's not prime as well. it's useful for larg nums;
 // but this was the easiest way
 
 console.log(isPrime(1));
@@ -33,5 +33,5 @@ console.log(isPrime(11));
 console.log(isPrime(true));
 console.log(isPrime('g'));
 console.log(isPrime(2347));
-console.log(isPrime(56387375173));
+// console.log(isPrime(56387375173));
 
