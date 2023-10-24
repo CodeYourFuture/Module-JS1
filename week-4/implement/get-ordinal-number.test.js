15515a7783ed1d520a37c1f99ef7c16f912bfcc4
Sariat <adenike.sariat@gmail.com>
2023-10-24 14:44:31 +0100

    writing condition to test for 'rd'

--- week-4/implement/get-ordinal-number.test.js
+++ week-4/implement/get-ordinal-number.test.js
@@ -7,6 +7,8 @@ function getOrdinalNumber(num) {
     return `${num}st`;
   } else if (num % 10 === 2 && num % 100 != 12) {
     return `${num}nd`;
+  } else if (num % 10 === 3 && num % 100 != 13) {
+    return `${num}rd`;
   }
 }
 
