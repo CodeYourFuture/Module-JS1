f865dfaed6aa9ab6916442c168fb53cb2e0fc6ef
LauraSantiag0 <eusoulaura8@gmail.com>
2023-10-24 22:54:43 +0100

    firt for week 4

--- week-4/implement/get-ordinal-number.test.js
+++ week-4/implement/get-ordinal-number.test.js
@@ -2,3 +2,30 @@
 
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
+
+function getOrdinalNumber(number){
+
+    if (number % 10 === 1 && number % 100 !== 11) {
+
+        return number + "st";
+    }
+
+    else if ( number % 10 === 2 && number % 100 !==12){
+
+        return number + 'nd';
+    }
+
+    else if (number % 10 === 3 && number %100 !== 13){
+
+        return number + 'rd'
+    }
+
+    else {
+
+        return number + 'th'
+    }
+}
+
+console.log (getOrdinalNumber(1))
+
+
