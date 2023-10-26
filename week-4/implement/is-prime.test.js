345f44c1f51616573fe057846914889842b7c89b
Sariat <adenike.sariat@gmail.com>
2023-10-26 12:27:12 +0100

    created if for less than 2 which also includes negative integer

--- week-4/implement/is-prime.test.js
+++ week-4/implement/is-prime.test.js
@@ -2,7 +2,9 @@
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
 function isPrime(num) {
-    
+   if (num < 2) {
+     return false;
+   } 
 }
 
 test("To check if a number is prime", () => {
