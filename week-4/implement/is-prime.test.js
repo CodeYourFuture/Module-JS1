e1d326fd8313f52b5b64b9992911572e56dd5b5b
Sariat <adenike.sariat@gmail.com>
2023-10-26 12:34:43 +0100

    wrote more test to check for the psoitive integers

--- week-4/implement/is-prime.test.js
+++ week-4/implement/is-prime.test.js
@@ -2,17 +2,21 @@
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
 function isPrime(num) {
-   if (num < 2) {
-     return false;
-   }
-   for (let index = 2; index <= Math.sqrt(num); index++) {
+  if (num < 2) {
+    return false;
+  }
+  for (let index = 2; index <= Math.sqrt(num); index++) {
     if (num % index === 0) {
       return false;
-    } 
-   }
+    }
+  }
+  return true;
 }
 
 test("To check if a number is prime", () => {
   expect(isPrime(1)).toBe(false);
   expect(isPrime(-84)).toBe(false);
+  expect(isPrime(3)).toBe(true);
+  expect(isPrime(12)).toBe(false);
+  expect(isPrime(1003)).toBe(false);
 });
