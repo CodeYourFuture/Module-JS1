4027e88b63b9b13ec83db877ab1f413d67cb3088
Sariat <adenike.sariat@gmail.com>
2023-10-26 12:38:09 +0100

    test passed and task completed

--- week-4/implement/is-prime.test.js
+++ week-4/implement/is-prime.test.js
@@ -1,6 +1,7 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
+
 function isPrime(num) {
   if (num < 2) {
     return false;
