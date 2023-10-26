3644e2f84aefdd986c6b29dfbdb16dfcf448739f
Sariat <adenike.sariat@gmail.com>
2023-10-26 12:24:52 +0100

    Writing test for the function

--- week-4/implement/is-prime.test.js
+++ week-4/implement/is-prime.test.js
@@ -1,3 +1,8 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
+
+
+test("To check if a number is prime", () => {
+  expect(isPrime(1)).toBe(false);
+});
