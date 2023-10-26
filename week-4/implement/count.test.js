a5845b492266f0ed0b36f3f8ef662e11e5d98280
karam <karam@karams-MacBook-Pro.local>
2023-10-26 14:13:08 +0100

    completed count.test.js

--- week-4/implement/count.test.js
+++ week-4/implement/count.test.js
@@ -15,3 +15,28 @@
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+function countChar(str, char) {
+    let index = 0;
+    let count = 0;
+    while (index < str.length) {          
+      if (str[index] == str[index + 1]) { 
+        if (str[index] === char) {        
+          count++;                        
+        }
+      }
+      index++;                            
+    }
+    return (count != 0) ? (count + 1) : 0;
+  }
+  
+  test("counts the number of times a character occurs in a string", function () {
+    expect(countChar("karaaam", "a")).toBe(3);
+    expect(countChar("aliii", "i")).toBe(3);
+    expect(countChar("kaamall", "i")).toBe(0);
+  });
+
+
+
+
+
--- week-4/investigate/find.js
+++ week-4/investigate/find.js
@@ -19,6 +19,8 @@ console.log(find("code your future", "z"));
 // Use the Python Visualiser to help you play computer with this example and observe how this code is executed
 // Pay particular attention to the following:
 
+//
+
 // a) How the index variable updates during the call to find
 // b) What is the if statement used to check
 // c) Why is index++ being used?
