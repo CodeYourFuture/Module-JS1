73ccc4193cd6a39d446b71c0da1660bd6d9bb2ad
Sariat <adenike.sariat@gmail.com>
2023-10-27 22:22:24 +0100

    introducing loop and creating more variables

--- week-4/implement/password-validator.test.js
+++ week-4/implement/password-validator.test.js
@@ -15,16 +15,42 @@ To be valid, a password must:
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
 function validPassword(input) {
-    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lower = "abcdefghijklmnopqrstuvwxyz";
-    const num = "0123456789";
-    const alphaSymbol = "!#$%.*&";
-    if (input.length >= 5) {
-      return "Oops! Make it up to or more than 5 characters.";
+  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+  const num = "0123456789";
+  const alphaSymbol = "!#$%.*&";
+
+  let validLength = false;
+  let isAlphaNumericSymbol = false;
+  let isUpper = false;
+  let isNum = false;
+  let isLower = false;
+
+  if (input.length >= 5) {
+    validLength = true;
+    // return "Oops! Make it up to or more than 5 characters.";
+  }
+  for (index = 0; index < input.length; index++) {
+    if (upper.includes(input[index])) {
+      isUpper = true;
     }
-
+  }
+  for (index = 0; index < input.length; index++) {
+    if (lower.includes(input[index])) {
+      isLower = true;
+    }
+  }
+  for (index = 0; index < input.length; index++) {
+    if (num.includes(input[index])) {
+      isNum = true;
+    }
+  }
+  for (index = 0; index < input.length; index++) {
+    if (alphaSymbol.includes(input[index])) {
+      isAlphaNumericSymbol = true;
+    }
+  }
 }
-
 
 test("This is to check the validity of user's password", () => {
   expect(validPassword("Olamide*6732")).toBe("Success!");
