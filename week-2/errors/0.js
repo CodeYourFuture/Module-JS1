31a9f4bbd82e383b697f90bd6655d2a25bd3dd3d
AbdihamidAli <abdi747573@gmail.com>
2023-11-03 13:17:13 +0000

    correcting words

--- week-2/errors/0.js
+++ week-2/errors/0.js
@@ -3,13 +3,13 @@
 /*
 The function takes a string as an argument and returns the capitalized first letter of the string.
 Inside the function,there is a new variable str is declared in which this will trigger an error because of 
-the The function "capitalise" has also defined with a single parameter str, 
+the The function "capitalize" has also defined with a single parameter str, 
 the error could be there is another variable called str and has already declared in the function parameter.
 or the parameter str and the locally declared str variable inside the function have the same name.
 to fix this we remove the keyword let as it has already been defined in the function parameter.
 */
-// call the function capitalise with a string input
-// there will be an error 
+// call the function capitalize with a string input
+// there will be an error
 
 // interpret the error message and figure out why an error is occurring
 //because of the let keyword that has been attached to a variable that has been defined.
