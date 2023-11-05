face722c4e7bbfdb0450267e168fabe21a19117e
Pedram Amani <amaaanip@gmail.com>
2023-11-05 20:29:38 +0000

    JS1-week1 | In exercises folder: paths.js done

--- week-1/exercises/paths.js
+++ week-1/exercises/paths.js
@@ -11,8 +11,27 @@
 
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
+
+console.log(lastSlashIndex);
+
 const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
+
+// const dirPart = "/Users/mitch/cyf/Module-JS1/week-1/interpret";
+
+// console.log(dirPart)
+
+const dirPart = filePath.slice(0, lastSlashIndex);
+
+console.log(`The dir part of the filePath is: ${dirPart}`);
+
+// const extOfTheFile = ".txt";
+
+// console.log(extOfTheFile);
+
+const extOfTheFile = filePath.slice(filePath.lastIndexOf("."));
+
+console.log(`The file's extension is: ${extOfTheFile}`);
