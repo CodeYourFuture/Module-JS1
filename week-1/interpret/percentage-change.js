2889a151dea2ca69339cd5f30ce15463cdfe45b2
areebsattar <areebsattar1@gmail.com>
2023-11-05 21:41:58 +0000

    completed interpret folder

--- week-1/interpret/percentage-change.js
+++ week-1/interpret/percentage-change.js
@@ -12,9 +12,13 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+// Ans. 4 calls in line 4 and 5 
 
 // b) Identify all the lines that are variable reassignment statements
+// Ans. Line 4 and 5
 
 // c) Identify all the lines that are variable declarations
+// Ans. Line 1,2,7 and 8
 
 // d) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// Ans. It is converting the string into a number and removing the comma from string before converting to number
--- week-1/interpret/to-pounds.js
+++ week-1/interpret/to-pounds.js
@@ -1,21 +1,21 @@
-const penceString = "399p";
+const penceString = "399p"; //initializes a string variable with the value "399p"
 
 const penceStringWithoutTrailingP = penceString.substring(
   0,
   penceString.length - 1
-);
+);    // Initializes a new variable to remove the last character from the string
 
-const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"); // Initializes a new variable to pad the whole number
 const pounds = paddedPenceNumberString.substring(
   0,
   paddedPenceNumberString.length - 2
-);
+); // New variable to pad the number to make value in pounds 
 
 const pence = paddedPenceNumberString
   .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
+  .padEnd(2, "0");  // new variable using function calls to convert the last 2 digits into pence by padding them
 
-console.log(`£${pounds}.${pence}`);
+console.log(`£${pounds}.${pence}`);   //Displaying the result with £ sign
 
 // This program takes a string representing a price in pence
 // The program then builds up a string representing the price in pounds
