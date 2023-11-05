06fc906f250129c10a67a127427285984cdadbcb
Pedram Amani <amaaanip@gmail.com>
2023-11-05 18:49:14 +0000

    JS1-week1 | In exercises folder: initials.js done

--- week-1/exercises/initials.js
+++ week-1/exercises/initials.js
@@ -4,3 +4,20 @@ let lastName = "Johnson";
 
 // Declare a variable called initials that stores the first character of each string in upper case to form the user's initials
 // Log the variable in each case
+
+const firstNameInitial = firstName.charAt(0).toUpperCase();
+
+console.log(firstNameInitial);
+
+const middleNameInitial = middleName.charAt(0).toUpperCase();
+
+console.log(middleNameInitial);
+
+const lastNameInitial = lastName.charAt(0).toUpperCase();
+
+console.log(lastNameInitial);
+
+const initials =
+  firstNameInitial + "." + middleNameInitial + "." + lastNameInitial + ".";
+
+console.log(initials);
