ca8461bd5d2f564b4863be53a4eb43e72d077e95
Pedram Amani <amaaanip@gmail.com>
2023-11-05 17:45:55 +0000

    JS1-week1 | In exercises folder: count.js done

--- week-1/exercises/count.js
+++ week-1/exercises/count.js
@@ -2,5 +2,9 @@ let count = 0;
 
 count = count + 1;
 
+console.log(count);
+
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+// = reassigns the value of variable "count" by adding "1" to the previous value of it
