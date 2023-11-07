e6a23a9c7dc8ca895bf52d11d34c567202eafdf8
zeliha pala <zelihapala78@gmail.com>
2023-11-07 22:47:35 +0000

    started errors

--- week-1/errors/1.js
+++ week-1/errors/1.js
@@ -1,4 +1,9 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+//const age = 33;
+//age = age + 1;
+
+//we can not reassign a new value to a constant. if  we reassign a value to a const variable, we will get a "TypeError." but we can do this;
+let age = 33;
 age = age + 1;
+console.log(age);
