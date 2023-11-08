d853fd417a680b9c79ae26f4120da7bbafafa4f1
OrlandoMoralesKuan <omoralesk@hotmail.com>
2023-11-08 18:16:25 +0000

    solving the initials exercise

--- week-1/exercises/initials.js
+++ week-1/exercises/initials.js
@@ -4,5 +4,14 @@ let lastName = "Johnson";
 
 // Declare a variable called initials that stores the first character of each string in upper case to form the user's initials
 // Log the variable in each case
-initials = firstName.charAt(0) + middleName.charAt(0) + lastName.charAt(0);
+
+initialFirstName = firstName.charAt(0);
+initialMiddleName = middleName.charAt(0);
+initialLastName = lastName.charAt(0);
+
+initials =
+  initialFirstName.toUpperCase() +
+  initialMiddleName.toUpperCase() +
+  initialLastName.toUpperCase();
+
 console.log(initials);
