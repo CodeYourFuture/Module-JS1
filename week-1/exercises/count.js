aa5e050bd1e76599f15f2949e8aaed0bf49885ad
Fathi Kahin <fhkahin@gmail.com>
2023-11-08 11:03:47 +0000

    initials updated

--- week-1/exercises/count.js
+++ week-1/exercises/count.js
@@ -5,7 +5,7 @@ count = count + 1;
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
 
-count = count + 1; //line three is increasing 0 by 1 the =  is the operator here
+count = count + 1; //line three is increasing 0 by 1 and the =  is the operator here.S
 
 
 
