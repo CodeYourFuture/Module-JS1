5ffdda30e8df1b2cc8ac4a781135905347daa26d
Fathi Kahin <fhkahin@gmail.com>
2023-11-08 10:51:36 +0000

    decimals updated

--- week-1/exercises/decimal.js
+++ week-1/exercises/decimal.js
@@ -6,3 +6,13 @@ const num = 56.5467;
 // Create a variable called wholeNumberPart and assign the whole number part of num to it
 const wholeNumberPart = Math.floor(num);
 
+// Create a variable called decimalPart and assign the decimal part of num to it
+const decimalPart = num - wholeNumberPart;
+
+// Create a variable called roundedNum and assign num rounded to the nearest whole number to it
+const roundedNum = Math.round(num);
+
+console.log("Whole Number Part:", wholeNumberPart);
+console.log("Decimal Part:", decimalPart);
+console.log("Rounded Number:", roundedNum);
+
