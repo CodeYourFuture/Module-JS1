cd40f5febe8e39bee72914b1d2aa7901a325b02f
RbAvci <rb.avci@gmail.com>
2023-11-10 18:09:07 +0000

    done to-pounds

--- week-1/interpret/to-pounds.js
+++ week-1/interpret/to-pounds.js
@@ -5,15 +5,22 @@ const penceStringWithoutTrailingP = penceString.substring(
   penceString.length - 1
 );
 
+console.log(penceStringWithoutTrailingP);
+
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+
+console.log(paddedPenceNumberString);
+
 const pounds = paddedPenceNumberString.substring(
   0,
   paddedPenceNumberString.length - 2
 );
 
-const pence = paddedPenceNumberString
-  .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
+console.log(pounds);
+
+const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+
+  console.log(pence);
 
 console.log(`£${pounds}.${pence}`);
 
@@ -27,3 +34,13 @@ console.log(`£${pounds}.${pence}`);
 // To begin, we can start with
 
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// const penceStringWithoutTrailingP = penceString.substring( 0,penceString.length - 1);
+// This line creates a new string (penceStringWithoutTrailingP) by using the substring method to extract a portion of penceString. 
+// It takes characters from the beginning (index 0) up to the length of penceString - 1, effectively removing the trailing "p".
+// Line 10 This line pads the penceStringWithoutTrailingP with leading zeros to ensure that it has at least three characters. The resulting string is stored in paddedPenceNumberString.
+// Line 14 Extracts the pounds part from paddedPenceNumberString by taking characters from the beginning up to the length of paddedPenceNumberString - 2.
+// Line 21 Extracts the last two characters (representing pence) from paddedPenceNumberString and then pads the result with trailing zeros using padEnd to ensure it has two characters.
+
+// Line 25 Prints the final formatted string by combining pounds, a dot, and pence, with a pound sign (£) at the beginning.
+
+
