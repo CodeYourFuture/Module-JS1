87779109b5521d45f44494bb70b7963d54ad64db
OrlandoMoralesKuan <omoralesk@hotmail.com>
2023-11-11 15:47:09 +0000

    debug 0.js finished

--- week-2/debug/0.js
+++ week-2/debug/0.js
@@ -5,3 +5,7 @@ function multiply(a, b) {
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// there is a function created called multiply and is waiting for 2 vaules (a, b), when we call it this function
+// and we declare the 2 values will print the multiplication for the 2 values, so here will multiply 10 * 32
+// give an answer the 320.
