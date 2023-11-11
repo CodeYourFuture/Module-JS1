7d6da826b1d32924bb7c42a0892e7cf374452c2a
areebsattar <areebsattar1@gmail.com>
2023-11-11 15:25:24 +0000

    Errors completed

--- week-2/errors/1.js
+++ week-2/errors/1.js
@@ -1,13 +1,17 @@
 // Predict and explain first...
+// Ans. decimalNumber not found
 // Write down the error you predict will be raised
 // Why will an error occur when this program runs?
+// Ans. Because the value of variable decimalNumber is fixed at 0.5 within function
 // Play computer with the example to work out what is going on
 
+let decimalNumber = 0.5
+
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  // const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(decimalNumber));
