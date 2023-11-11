edb6a0847b146c4e81724209e575ca838f51d7d1
RbAvci <rb.avci@gmail.com>
2023-11-11 14:37:14 +0000

    done debug

--- week-2/debug/0.js
+++ week-2/debug/0.js
@@ -1,7 +1,11 @@
 // Predict and explain first...
 
 function multiply(a, b) {
-  console.log(a * b);
+  return (a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+
+// The result will be undefined with console.log in function
+// we need to return instead of console.log 
--- week-2/debug/2.js
+++ week-2/debug/2.js
@@ -2,7 +2,7 @@
 
 const num = 103;
 
-function getLastDigit() {
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
@@ -11,4 +11,8 @@ console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
+
 // Explain why getLastDigit is not working properly - correct the problem
+
+// Because getLastDigit function gets global input num=103 always when its empty. So we should write "num" to evaluate 42, 105 or 806.
+// Then it works
