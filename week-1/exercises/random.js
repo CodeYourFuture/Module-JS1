28e158b3c77268dc621e8c0ccfe47b06adf39f96
Nazanin <126393077+nazaninsaedi@users.noreply.github.com>
2023-11-11 09:09:11 +0000

    week1

--- week-1/exercises/random.js
+++ week-1/exercises/random.js
@@ -9,6 +9,5 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 //**(maximum - minimum + 1): This calculates the range of numbers between maximum and minimum (both inclusive). 
 //**Adding 1 ensures that the upper bound is included in the range. */
-
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num several times to build an idea of what the program is doing
