a6dc77b8052ac0626e0e9afeec056c4eea606c33
RbAvci <rb.avci@gmail.com>
2023-11-11 15:09:38 +0000

    done errors

--- week-2/errors/2.js
+++ week-2/errors/2.js
@@ -3,8 +3,10 @@
 // this function should square any number but instead we're going to get an error
 // what is happening? How can we fix it?
 
-function square(3) {
+function square(num) {
     return num * num;
 }
 
+console.log(square(3));
 
+// we need to change parameter 3 to "num" and write as console.log(square(3));
