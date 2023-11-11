a11d45daa19fc2692c32b831cbaf3565552c570d
Nazanin <126393077+nazaninsaedi@users.noreply.github.com>
2023-11-11 09:11:09 +0000

    week1answers

--- week-1/exercises/random.js
+++ week-1/exercises/random.js
@@ -5,7 +5,8 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
 // In this exercise, you will need to work out what num represents?
 //* num represents a random integer between 1 and 100 (inclusive). 
-
+//Math.floor(...): This rounds down the result of the expression inside it to the nearest integer. 
+//This is done to ensure that the final result is an integer.
 // Try breaking down the expression and using documentation to explain what it means
 //**(maximum - minimum + 1): This calculates the range of numbers between maximum and minimum (both inclusive). 
 //**Adding 1 ensures that the upper bound is included in the range. */
