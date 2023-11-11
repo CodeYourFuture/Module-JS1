9426b070db30d472c4bfdfb6854e48412bfcd679
Fathi Kahin <fhkahin@gmail.com>
2023-11-11 16:15:56 +0000

    capitalized str

--- week-2/errors/0.js
+++ week-2/errors/0.js
@@ -2,7 +2,24 @@
 // write down the error you predict will be raised
 // then call the function capitalise with a string input
 // interpret the error message and figure out why it's happening, if your prediction was wrong
+// function capitalise(str) {
+//  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+
+//....................................
+// the problem is that  (str) is already the input to the function
+
 function capitalise(str) {
- let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let capitalizedStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalizedStr;
 }
+
+const originalString = "i love cyf"; 
+const capitalizedString = capitalise(originalString);
+
+console.log(`Original: ${originalString}`);
+console.log(`Capitalized: ${capitalizedString}`);
+
+
+
