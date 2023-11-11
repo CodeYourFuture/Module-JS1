d872c9d7c9823ab04344e9ad444bc94d7f40200b
Fathi Kahin <fhkahin@gmail.com>
2023-11-11 15:38:27 +0000

    NW6 | FATHI_KAHIN | JS1 | week 2 exercises | WEEK2

--- week-2/debug/1.js
+++ week-2/debug/1.js
@@ -1,8 +1,17 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+ return;
+ a + b }
+
+//console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// the semi colon in the return statement will stop the function execution. it should be as follows:
+
+function sum(a, b) {
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+
--- week-2/debug/2.js
+++ week-2/debug/2.js
@@ -1,14 +1,25 @@
 // Predict and explain first...
 
-const num = 103;
+// const num = 103;
 
-function getLastDigit() {
-  return num.toString().slice(-1);
-}
+// function getLastDigit() {
+//  return num.toString().slice(-1); }
+
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// This program should tell the user the last digit of each number.
+// Explain why getLastDigit is not working properly - correct the problem
+//...........................................................................
+// first the (num) is not defined, second the (const num = 103;) is a fixed number and that is what it will 
+//          calculate for all numbers, their  last digit will remain 3, we can fix this by using
+//          the  following code:
+
+function getLastDigit(num) {
+  return num.toString().slice(-1); }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
-// This program should tell the user the last digit of each number.
-// Explain why getLastDigit is not working properly - correct the problem
