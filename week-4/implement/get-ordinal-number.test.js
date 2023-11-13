3ff682f34a88409f01cf5e2e66fe04b7eedd4895
berwin eibo <perween.kurd1@gmail.com>
2023-11-13 00:40:30 +0000

    Get-ordinal number-task

--- week-4/implement/get-ordinal-number.test.js
+++ week-4/implement/get-ordinal-number.test.js
@@ -2,3 +2,19 @@
 
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
+
+function getOrdinalNumber(){
+   return "2nd";
+}
+Test("convert 2 to an ordinal number", function(){
+  const input = 2;
+  const currentOutput =getOrdinalNumber(input);
+  const targetOutput = "2nd";
+
+  expect(currentOutput).toBe(targetOutput);
+})
+  
+  
+  
+  
+ 
