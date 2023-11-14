0d7364cf8bd2f6a033aba13c22074311919bac41
fikret ellek <fikret.ellek@gmail.com>
2023-11-14 16:42:23 +0000

    week 1 completed

--- week-1/exercises/random.js
+++ week-1/exercises/random.js
@@ -3,7 +3,11 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
-// In this exercise, you will need to work out what num represents?
+// In this exercise, you will need to work out what num represents? it will give us a random number between max end min, including max and min
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num several times to build an idea of what the program is doing
+
+// it is giving us a random integer number between min and max including min and math
+
+console.log(num);
--- week-1/interpret/percentage-change.js
+++ week-1/interpret/percentage-change.js
@@ -13,8 +13,16 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
+    // 3 functions, 4-5-10
+
 // b) Identify all the lines that are variable reassignment statements
+
+    // 4-5
 
 // c) Identify all the lines that are variable declarations
 
+    // 1-2-7-8
+
 // d) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+    // it takes the carPrice string and replaces all "'" with "" so it gives us only number characters as a string, then with Number function string becomes integer
