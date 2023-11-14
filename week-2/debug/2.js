49bb69d97ac7ef67dadee7d20d11f2ba796d7ce7
Fathi Kahin <fhkahin@gmail.com>
2023-11-14 18:26:21 +0000

    debur and aerrors udated

--- week-2/debug/2.js
+++ week-2/debug/2.js
@@ -2,8 +2,8 @@
 
 // const num = 103;
 
-// function getLastDigit() {
-//  return num.toString().slice(-1); }
+function getLastDigit() {
+return num.toString().slice(-1); }
 
 // console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 // console.log(`The last digit of 105 is ${getLastDigit(105)}`);
