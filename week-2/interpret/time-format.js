a9e5249ac170b1d968315dd8058e9dad64c265da
Pedro <riccip@outlook.com>
2023-11-14 15:40:43 +0000

    week 2 all done

--- week-2/interpret/time-format.js
+++ week-2/interpret/time-format.js
@@ -7,11 +7,13 @@ function pad(num) {
 
 function formatTimeDisplay(seconds) {
   const remainingSeconds = seconds % 60;
+  console.log('remainingSeconds:', remainingSeconds);
   const totalMinutes = (seconds - remainingSeconds) / 60;
   const remainingMinutes = totalMinutes % 60;
+  console.log('remainingMinutes', remainingMinutes);
   const totalHours = (totalMinutes - remainingMinutes) / 60;
   const remainingHours = totalHours % 24;
-
+  console.log('remainingHours', remainingHours);
   return `${pad(remainingHours)}:${pad(remainingMinutes)}:${pad(
     remainingSeconds
   )}`;
@@ -26,18 +28,23 @@ console.log(formatTimeDisplay(143));
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-
+// 3
 // Call formatTimeDisplay with an input of 143, now answer the following:
 
 // b) What value is assigned to the parameter num when pad is called for the first time?
+// 23
 
 // c) What is the return value of pad when it is called for the first time?
+// 23
 
 // d) What is the value assigned to the parameter num when pad
 // is called for the last time in this program?  Explain your answer
+// 0, because 143 seconds is less than 1 hour.
 
 // e) What is the return value when pad is called
 // for the last time in this program?  Explain your answer
+// 00, because 0 is less than 10 and the if statement add a 0 as string.
 
 // f) Research an alternative way of padding the numbers in this code.
 // Look up the string functions on mdn
+// Similar we used in to-pounds.js, we can pad the numbers with padStart(2, '0')
