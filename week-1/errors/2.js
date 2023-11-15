b0fae8aeda8e6f638659e8d9139cfc9df35422e0
BakhatBegum <bakhtbagum@gmail.com>
2023-11-15 15:03:14 +0000

    add some code in function body

--- week-1/errors/2.js
+++ week-1/errors/2.js
@@ -1,5 +1,5 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
-// Ans: Before console log a variable we need to define it first.
+// Ans: Before log a variable we need to define it first.
 
 // what's the error ?
 //ans : In JavaScrip/node.js the code executed from top to bottom, any variable must be declare before log in to console.
--- week-1/exercises/paths.js
+++ week-1/exercises/paths.js
@@ -17,14 +17,16 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 
 const directory = filePath.slice(0, lastSlashIndex);
-console.log(`The base part of ${filePath} is ${directory}`);
+console.log(`The dir part of ${filePath} is ${directory}`);
 
 
 // Create a variable to store the ext part of the variable
 
+
+
 const extPath = base.lastIndexOf(".");
 const ext = extPath !== -1? base.slice(extPath + 1): "No extension";
-console.log(`The base part of ${filePath} is ${ext}`);
+console.log(`The ext part of ${filePath} is ${ext}`);
 
 
 
