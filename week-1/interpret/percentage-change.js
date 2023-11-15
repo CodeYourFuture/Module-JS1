c19c72b06802b9557b5eec5ed2d6bc44b184ab00
BakhatBegum <bakhtbagum@gmail.com>
2023-11-15 16:38:03 +0000

    call function

--- week-1/interpret/percentage-change.js
+++ week-1/interpret/percentage-change.js
@@ -13,7 +13,9 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
-//ans; 3 functions, line 4 , 5 and 10.
+//ans; in total four
+//2 calls to Number()
+//2 calls to replaceAll()
 
 // b) Identify all the lines that are variable reassignment statements
 
