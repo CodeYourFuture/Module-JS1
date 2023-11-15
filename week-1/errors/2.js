8b2615d7050976dc346ce55d5dfe8776e0f51973
BakhatBegum <bakhtbagum@gmail.com>
2023-11-15 14:53:51 +0000

    we can solve it my add // before a line of code.

--- week-1/errors/2.js
+++ week-1/errors/2.js
@@ -1,6 +1,11 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
+// Ans: Before console log a variable we need to define it first.
+
 // what's the error ?
-//ans : In JavaScrip/node.js the code executed from top to bottom, any variable must be declare before log it to console.
+//ans : In JavaScrip/node.js the code executed from top to bottom, any variable must be declare before log in to console.
 
 const cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
+
+console.log(`I was born in ${cityOfBirth}`);
+const cityOfBirth = "Bolton";
