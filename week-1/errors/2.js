9ab53a9e8983f1055ed76d2a4ce2feb82fed224c
BakhatBegum <bakhtbagum@gmail.com>
2023-11-15 15:42:13 +0000

    define variable be for log it

--- week-1/errors/2.js
+++ week-1/errors/2.js
@@ -4,8 +4,9 @@
 // what's the error ?
 //ans : In JavaScrip/node.js the code executed from top to bottom, any variable must be declare before log in to console.
 
-const cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
+const cityOfBirth = "Bolton";
 
-console.log(`I was born in ${cityOfBirth}`);
+// here is the correct code
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
