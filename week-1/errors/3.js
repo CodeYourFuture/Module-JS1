6854f632c6df4dfb5d1625f74990e5d9d4555172
BakhatBegum <bakhtbagum@gmail.com>
2023-11-15 15:55:52 +0000

    const

--- week-1/errors/3.js
+++ week-1/errors/3.js
@@ -6,5 +6,5 @@ console.log(last4Digits);
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
 // Make and explain a prediction about why the code won't work
-//ans: I think we use .slice method for string. According to me, .toFixed() method will work on card. The method .toFixed() is used for numerical values .
+//ans: because we used const to declared variable and we can not reassign variable by using const.
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
