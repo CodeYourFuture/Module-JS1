8942482f3daf6a2cb451601083f437f1a80d2f77
Fathi Kahin <fhkahin@gmail.com>
2023-11-15 18:46:49 +0000

    vat.js answered

--- week-2/implement/vat.js
+++ week-2/implement/vat.js
@@ -8,3 +8,17 @@
 // Given a number,
 // When I call this function with a number
 // Then it returns the new price with VAT added on
+//.........................................................
+
+function calculatePriceWithVAT(originalPrice) {
+    const vatRate = 1.2; // 20% VAT rate
+    const priceWithVAT = originalPrice * vatRate;
+    return priceWithVAT;
+  }
+
+  const originalPrice = 50;
+  const newPrice = calculatePriceWithVAT(originalPrice);
+  
+  console.log(`Original Price: £${originalPrice}`);
+  console.log(`New Price with VAT: £${newPrice.toFixed(2)}`);
+  
