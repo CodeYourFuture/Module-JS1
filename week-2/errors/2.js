3dc7cc7204324fa5b0c84f007f774a08c91558e6
Fathi Kahin <fhkahin@gmail.com>
2023-11-15 18:48:32 +0000

    2.js commented out

--- week-2/errors/2.js
+++ week-2/errors/2.js
@@ -3,9 +3,9 @@
 // this function should square any number but instead we're going to get an error
 // what is happening? How can we fix it?
 
-function square(3) {
-    return num * num;
-   }
+// function square(3) {
+//     return num * num;
+//    }
 
 //...........................................
 
