933161cf2090c91bf417cba2b3a1069c334d2041
AbdihamidAli <abdi747573@gmail.com>
2023-11-16 05:18:21 +0000

    JS1-week4-isPrime task

--- week-4/implement/is-prime.test.js
+++ week-4/implement/is-prime.test.js
@@ -1,3 +1,23 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= num / 2; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const numToCheck = 9;
+if (isPrime(numToCheck)) {
+  console.log(`${numToCheck} is prime.`);
+} else {
+  console.log(`${numToCheck} is not prime.`);
+}
