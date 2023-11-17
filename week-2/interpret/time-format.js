6142976c0e08180aac564207e2415b14e6a3faa8
areebsattar <areebsattar1@gmail.com>
2023-11-17 19:43:10 +0000

    interpret completed

--- week-2/interpret/time-format.js
+++ week-2/interpret/time-format.js
@@ -26,18 +26,23 @@ console.log(formatTimeDisplay(143));
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
+// Ans. 3
 
 // Call formatTimeDisplay with an input of 143, now answer the following:
 
 // b) What value is assigned to the parameter num when pad is called for the first time?
-
+//Ans. 0
 // c) What is the return value of pad when it is called for the first time?
-
+// Ans. "00"
 // d) What is the value assigned to the parameter num when pad
 // is called for the last time in this program?  Explain your answer
+//Ans. 
 
 // e) What is the return value when pad is called
 // for the last time in this program?  Explain your answer
-
+//Ans. The return value is 23 and the reason that it wasn't padded with 0 is because the 
+//parameter num value was greater than 10 in which case function didn't return with 0 at start
 // f) Research an alternative way of padding the numbers in this code.
 // Look up the string functions on mdn
+//Ans. We can use javascript string.padStart function to pad the number with 0 in 
+//our case rather than defining our own function
