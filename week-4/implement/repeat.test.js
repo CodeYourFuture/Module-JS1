192768bb6d5fef754fb02d7adefe26ceedb3fac1
AbdihamidAli <abdi747573@gmail.com>
2023-11-17 02:20:55 +0000

    js1-week4-repeat function with jest

--- week-4/implement/repeat.test.js
+++ week-4/implement/repeat.test.js
@@ -23,3 +23,30 @@
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+
+function repeat(str, count) {
+  if (typeof str !== "string" || typeof count !== "number") {
+    return `Invalid input types. Please make this ${str} a string if not and this ${count} a number if not.`;
+  }
+
+  if (count < 0) {
+    return "Negative count is not valid";
+  }
+
+  let result = "";
+  for (let i = 0; i < count; i++) {
+    result += `${str[0].toUpperCase()}${str.slice(1)}`;
+  }
+
+  return result;
+}
+
+// console.log(repeat("test", 3));
+
+test("Valid input", () => {
+  expect(repeat("test", 3)).toBe("TestTestTest");
+  expect(repeat(123, 3)).toBe(
+    "Invalid input types. Please make this 123 a string if not and this 3 a number if not."
+  );
+  expect(repeat("test", -1)).toBe("Negative count is not valid");
+});
