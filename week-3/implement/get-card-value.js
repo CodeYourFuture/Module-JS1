444de99c605247638902f9edfe0a22cba04ff074
Pedro <riccip@outlook.com>
2023-11-20 08:29:38 +0000

    get-card-value done, assertion for wrong card suit added

--- week-3/implement/get-card-value.js
+++ week-3/implement/get-card-value.js
@@ -29,3 +29,52 @@
 // Given a card with an invalid rank (neither a number nor a recognized face card),
 // When the function is called with such a card,
 // Then it should throw an error indicating "Invalid card rank."
+
+function getCardValue(card) {
+  cardSuit = card.slice(-1);
+  cardRank = card.slice(0, -1);
+
+  if (
+    cardSuit == "♡" ||
+    cardSuit == "♢" ||
+    cardSuit == "♣" ||
+    cardSuit == "♠"
+  ) {
+    switch (true) {
+      case cardRank == "A":
+        return 11;
+        break;
+
+      case cardRank > 1 && cardRank < 11:
+        return 5;
+        break;
+
+      case cardRank == "J" || cardRank == "Q" || cardRank == "K":
+        return 10;
+        break;
+
+      default:
+        return `Invalid card rank.`;
+        break;
+    }
+  } else {
+    return "Invalid card suit. Please use one of the following: ♡,♢,♣,♠.";
+  }
+}
+
+function assertion(card, targetOutput) {
+  const currentOutput = getCardValue(card);
+  console.assert(
+    currentOutput === targetOutput,
+    `Test for ${card}. Current output: %s, target output: %s`,
+    currentOutput,
+    targetOutput
+  );
+}
+
+assertion("8♦", "Invalid card suit. Please use one of the following: ♡,♢,♣,♠.");
+assertion("13♣", "Invalid card rank.");
+assertion("A♠", 11);
+assertion("4♢", 5);
+assertion("10♡", 5);
+assertion("Q♣", 10);
