3ffec3548c463262a76df376795fab0a65f114a9
Pedro <riccip@outlook.com>
2023-11-20 09:55:01 +0000

    debug/2.js problem solved

--- week-2/debug/1.js
+++ week-2/debug/1.js
@@ -3,8 +3,7 @@
 // will be wrong (undefined), as it will not return anything (look the semicolon after return).
 
 function sum(a, b) {
-  return;
-  a + b;
+  return  a + b;
 }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}.`);
--- week-2/debug/2.js
+++ week-2/debug/2.js
@@ -2,15 +2,15 @@
 // The function always returns 3, because num is a constant and the function doesn't expect any input.
 // So, the console.log will print the string correctly, but the number will be always 3 (ignoring the input)
 
-const num = 103;
 
-function getLastDigit() {
-  return num.toString().slice(-1);
+
+function getLastDigit(number) {
+  return number.toString().slice(-1);
 }
 
-console.log(`The last digit of 42 is ${getLastDigit(42)}`);
-console.log(`The last digit of 105 is ${getLastDigit(105)}`);
-console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+console.log(`The last digit of 42 is ${getLastDigit(42)}.`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}.`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}.`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
