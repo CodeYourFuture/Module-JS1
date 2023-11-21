29d74dff8e015110f32db375dd7509c4804cea91
Your Name <you@example.com>
2023-11-21 20:23:45 +0000

    debug

--- week-2/debug/1.js
+++ week-2/debug/1.js
@@ -1,8 +1,9 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+the error here that we should place the a + b expression directly after the return keyword.
