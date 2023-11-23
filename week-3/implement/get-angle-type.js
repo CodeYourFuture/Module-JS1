4b5f604a4b34d905e4a4af0c5c67596ec50f367a
BakhatBegum <bakhtbagum@gmail.com>
2023-11-23 10:40:13 +0000

    changed return value

--- week-3/implement/get-angle-type.js
+++ week-3/implement/get-angle-type.js
@@ -27,7 +27,7 @@ function getAngleType(angle){
     if (angle === 90){
         return "Right angle";
     } else if (angle != 90 && angle < 90){
-        return "Right angle";
+        return "Acute angle";
     }else if (angle > 90 && angle < 180){
         return "Obtuse angle";
     }else if (angle <= 180 && angle === 180){
