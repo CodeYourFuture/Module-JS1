f115c6fcc94e6184aacaa612fbf78cfa8607fd7e
BakhatBegum <bakhtbagum@gmail.com>
2023-11-26 17:26:00 +0000

    added some lines of code

--- week-3/stretch/rotate-char.js
+++ week-3/stretch/rotate-char.js
@@ -17,6 +17,12 @@
 // When the function is called with these inputs,
 // Then it should rotate the lowercase letter by shift positions within the lowercase alphabet, wrapping around if necessary, and return the rotated lowercase letter as a string.
 
+function rotateCharacter(char, shift) {
+    let character = char.toLowerCase();
+    let shiftedCharCode = (character.charCodeAt(0) - 97 + shift) % 26 + 97;
+    return String.fromCharCode(shiftedCharCode);
+}
+
 console.log(rotateCharacter("a", 3)); // Output: "d"
 console.log(rotateCharacter("f", 1)); // Output: "g"
 
@@ -25,6 +31,12 @@ console.log(rotateCharacter("f", 1)); // Output: "g"
 // When the function is called with these inputs,
 // Then it should rotate the uppercase letter by shift positions within the uppercase alphabet, wrapping around if necessary, and return the rotated uppercase letter as a string.
 
+
+function rotateCharacter(char, shift) {
+    let character = char.toUpperCase();
+    let shiftedCharCode = (character.charCodeAt(0) - 97 + shift) % 36 + 97;
+    return String.fromCharCode(shiftedCharCode);
+}
 console.log(rotateCharacter("A", 3)); // Output: "D"
 console.log(rotateCharacter("F", 1)); // Output: "G"
 
@@ -35,6 +47,18 @@ console.log(rotateCharacter("F", 1)); // Output: "G"
 // This specification outlines the behavior of the rotateCharacter function for different input scenarios, including valid and invalid characters, and defines the expected output or action for each case.
 
 
+function rotateCharacter(char, shift) {
+    if (/[0-9]/.test(char)) {
+        // If the character is a digit, return it as is
+        return char;
+    }
+
+    let lowerCaseChar = char.toLowerCase();
+    let shiftedCharCode = (lowerCaseChar.charCodeAt(0) - 97 + shift) % 26 + 97;
+    return String.fromCharCode(shiftedCharCode);
+}
+
+console.log(rotateCharacter("2", 3));  // Output: 2
 console.log(rotateCharacter("7", 5)); // Output: "7" (unchanged, not a letter)
 
 // Scenario: Shifting a Character with Wraparound
@@ -44,7 +68,14 @@ console.log(rotateCharacter("7", 5)); // Output: "7" (unchanged, not a letter)
 // Then it should correctly rotate the character by shift positions within the alphabet while handling the wraparound,
 // And the function should return the rotated character as a string (e.g., 'z' rotated by 3 should become 'c', 'Z' rotated by 3 should become 'C').
 
+function rotateCharacter(char, shift) {
+    let character = char.toLowerCase();
+    let shiftedCharCode = (character.charCodeAt(0) - 97 + shift) % 26 + 97;
+    return String.fromCharCode(shiftedCharCode);
+}
 
+
+console.log(rotateCharacter("z", 3));  // Output: "c"
 console.log(rotateCharacter("z", 1)); // Output: "a" (unchanged, not a letter)
 
 
