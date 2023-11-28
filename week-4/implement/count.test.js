fd91469a105ebc9eb9c76391b08ec656ae7f0f0a
haythem-f <haythem00@gmail.com>
2023-11-28 06:41:39 +0000

    Implemment-count

--- week-4/implement/count.test.js
+++ week-4/implement/count.test.js
@@ -15,3 +15,19 @@
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+function countChar(str, char) {
+    let counter = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+const myString = "My Name Is Haythem Farog Mohammed, I am a Sudanese, and live in Bolton!";
+const charToCount = 'o';
+const result = countChar(myString, charToCount);
+
+console.log(`The character '${charToCount}' occurs ${result} times in the string.`);
