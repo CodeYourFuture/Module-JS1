63a3f8811db0179d4224827b00d58df0fead9b0c
haythem-f <haythem00@gmail.com>
2023-11-28 19:05:24 +0000

    Implement-isPrime

--- week-4/implement/is-prime.test.js
+++ week-4/implement/is-prime.test.js
@@ -1,3 +1,30 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should return a boolean representing whether the num is prime
+
+function isPrime(number) {
+    let isPrime = true;
+  
+    if (number === 1) {
+      console.log("1 is neither prime nor composite number.");
+  }
+  else if (number > 1) {
+      for (let i = 2; i < number; i++) {
+          if (number % i == 0) {
+              isPrime = false;
+              break;
+          }
+      }
+  
+      if (isPrime) {
+          console.log(`${number} is a prime number`);
+      } else {
+          console.log(`${number} is a not prime number`);
+      }
+  }
+  else {
+      console.log("The number is not a prime number.");
+  }
+  }
+  isPrime(3);
+  
