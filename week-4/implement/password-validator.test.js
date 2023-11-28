cb3f7a0f78b8ed4600bf6105b2705579e6b4154c
haythem-f <haythem00@gmail.com>
2023-11-28 21:02:28 +0000

    Implement-passValidat

--- week-4/implement/password-validator.test.js
+++ week-4/implement/password-validator.test.js
@@ -14,3 +14,22 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+const regex = /[^\w\s']/g;
+function passValidat(pass) {
+    if (pass.length < 5) 
+    {console.log("Password must be 5 or more");}
+      
+    else if(pass.search(/[a-z]/) < 0) 
+    { console.log("Error: Password must contain at least one lowercase letter");}
+      
+    else if(pass.search(/[A-Z]/) < 0) 
+    {console.log("Error: Password must contain at least one uppercase letter");} 
+    
+    else if(pass.search(/[0-9]/) < 0) {console.log("Error: Password must contain at least one number");} 
+    
+    else if(pass.search(regex) < 0) {console.log("Error: Password must contain at least one Symbol");}
+
+    else {console.log("Valid!");}
+      
+  }
+  passValidat("1a1*1F1");
