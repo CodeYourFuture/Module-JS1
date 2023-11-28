79ba72a00f76d30c22e6611329f42f0251bc9aee
OrlandoMoralesKuan <omoralesk@hotmail.com>
2023-11-28 14:12:59 +0000

    is prime.test.js exercise from implement folder is done

--- week-4/implement/is-prime.test.js
+++ week-4/implement/is-prime.test.js
@@ -1,3 +1,16 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should return a boolean representing whether the num is prime
+
+function isPrime(number) {
+  if (number > 0 && number % 2 == 0) {
+    return true;
+  }
+  if (number > 0 && number % 2 != 0) {
+    return false;
+  } else {
+    return "Insert a positive number";
+  }
+}
+
+console.log(isPrime(2));
