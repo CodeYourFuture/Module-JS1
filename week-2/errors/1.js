f92e05c3b444784390c61fad2818326b7f6ebd1d
Your Name <you@example.com>
2023-11-30 18:54:14 +0000

    1.js errors

--- week-2/errors/1.js
+++ week-2/errors/1.js
@@ -2,12 +2,14 @@
 // Write down the error you predict will be raised
 // Why will an error occur when this program runs?
 // Play computer with the example to work out what is going on
+//In this code, there is an error that the variable decimalNumber, declared twice. once as a parameter of the convertToPercentage function and again outside the function, because in JS variables can't be declared twice within the same scope
+// to fix this error, we need to delete the parameter in line 8, so we can call the function without any argument.
 
-function convertToPercentage(decimalNumber) {
+function convertToPercentage() {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage());
