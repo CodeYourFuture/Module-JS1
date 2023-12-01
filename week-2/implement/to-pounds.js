0fa188a55811453f4b778894e020f42c18f01119
Sabella-8 <sabella549@gmail.com>
2023-12-01 22:20:48 +0000

    card validator added

--- week-2/implement/to-pounds.js
+++ week-2/implement/to-pounds.js
@@ -3,8 +3,21 @@
 // Take this code and turn it into a reusable block of code.
 // Declare a function called toPounds with an appropriately named parameter.
 // Call this function a number of times to check it works for different inputs
-function toPoud(Birr){
-return birr / 45 ;
+const penceString = "399p";
 
-}
-console.log(toPound(50));
+const penceStringWithoutTrailingP = penceString.substring(
+  0,
+  penceString.length - 1
+);
+
+const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+const pounds = paddedPenceNumberString.substring(
+  0,
+  paddedPenceNumberString.length - 2
+);
+
+const pence = paddedPenceNumberString
+  .substring(paddedPenceNumberString.length - 2)
+  .padEnd(2, "0");
+
+console.log(`£${pounds}.${pence}`);
