0d9a980a8690ad2bdf287f8f5a8016a18544def2
BakhatBegum <bakhtbagum@gmail.com>
2023-12-01 11:13:01 +0000

    added test case on count.js

--- week-4/implement/count.test.js
+++ week-4/implement/count.test.js
@@ -15,3 +15,15 @@
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+const countChar = require("./count");
+
+
+
+test('When the function is called with these inputs,Then it should correctly count consecutive occurrences of char (e.g., \'a\' appears five times in \'aaaaa\'). ". ', () => {
+
+    expect(countChar("a", 3)).toBe("aaa");
+    
+});
+
+
