150892bdb2b8c221e8ae0faec776d3c44ec4b549
Your Name <you@example.com>
2023-12-01 15:19:29 +0000

    changes on the debug file

--- week-2/debug/1.js
+++ week-2/debug/1.js
@@ -6,4 +6,4 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-the error here that we should place the a + b expression directly after the return keyword.
+//the error here that we should place the a + b expression directly after the return keyword.
