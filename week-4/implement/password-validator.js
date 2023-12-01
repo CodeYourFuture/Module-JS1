fc8fe13300df4d5f41cfeaaa32bef9394cca023d
BakhatBegum <bakhtbagum@gmail.com>
2023-12-01 11:18:06 +0000

    where was an error on code.

---
+++ week-4/implement/password-validator.js
@@ -0,0 +1,29 @@
+function passwordValidator(password,oldPassword){
+    if (password.length >= 5){
+        return false;
+    }
+    else if (!/[A-Z]/.test(password)){
+        return false;
+    }else if(!/[a-z]/.test(password)){
+        return false;
+    }else if(!/[0-9]/.test(password)){
+        return false;
+    }else if(password == "!" || password == "$" || password == "*" || password == "." || password == "#" || password == "&" || password == "%"){
+        return false;
+    }
+    else if (oldPassword.includes(password)){
+        return false
+    }
+    else {
+        return true;
+    }
+
+}
+
+const oldPassword = ("bbds", "flbe8")
+const password = ("khadIja2")
+
+//console.log(passwordValidator(p, op))
+
+
+module.exports = passwordValidator;
