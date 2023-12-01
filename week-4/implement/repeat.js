45badd5dd23fb8a5ca12328d060aab695378d01f
BakhatBegum <bakhtbagum@gmail.com>
2023-12-01 11:20:27 +0000

    added for loop

---
+++ week-4/implement/repeat.js
@@ -0,0 +1,13 @@
+function repeat(str, count){
+    if(count < 0){
+        return "negative counts are not valid";
+    }
+
+else if (str.repeat(count) !== '') {
+        return str.repeat(count);
+}
+return ;
+};
+//console.log(repeat("Bgft", -8)) 
+
+module.exports = repeat;
