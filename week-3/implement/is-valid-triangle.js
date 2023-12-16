a564df85c91813e98156923bacedb6f8c60a294a
Your Name <you@example.com>
2023-12-16 15:47:19 +0000

    valid triangle

--- week-3/implement/is-valid-triangle.js
+++ week-3/implement/is-valid-triangle.js
@@ -38,3 +38,12 @@
 // Then it should return true because the input forms a valid triangle.
 
 // This specification outlines the behavior of the isValidTriangle function for different input scenarios, ensuring it properly checks for invalid side lengths and whether they form a valid triangle according to the Triangle Inequality Theorem.
+
+function isValidTriangle(a, b, c) {
+  if (a + b <= c || a + c <= b || b + c <= a) {
+    return false; // Invalid triangle
+  }
+  return true; // Valid triangle
+}
+const isTriangleValid = isValidTriangle(3, 4, 5);
+console.log(isTriangleValid); // Output: true (valid triangle)
