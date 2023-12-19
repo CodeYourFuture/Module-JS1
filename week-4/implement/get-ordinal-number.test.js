ca444379048a633398396d69267319581cf6f684
Bakhat Begum <bakhtbagum@gmail.com>
2023-12-19 19:15:57 +0000

    The test case order has been changed.

--- week-4/implement/get-ordinal-number.test.js
+++ week-4/implement/get-ordinal-number.test.js
@@ -7,6 +7,16 @@ test("when the function is called, it should bring num with suffix of \"st\", \"
     expect(getOrdinalNumber(1)).toBe("1st");
 })
 
+
+test("when the function is called, it should bring num with suffix of \"st\", \"en\", \"rd\", and \"th\".", () => {
+
+    expect(getOrdinalNumber(11)).toBe("11th");
+})
+
+test("when the function is called, it should bring num with suffix of \"st\", \"en\", \"rd\", and \"th\".", () => {
+
+    expect(getOrdinalNumber(21)).toBe("21st");
+})
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
 
